3d0680351c82436239dbb61b50ac9f4c9fd7fba2
kubakhov <kubakhov.dmitry@gmail.com>
2019-04-30 15:00:07 +0200

    add new rule type -> FileParsing

---
+++ sail-point-improved/src/main/java/com/sailpoint/improved/rule/connector/FileParsingRule.java
@@ -0,0 +1,147 @@
+package com.sailpoint.improved.rule.connector;
+
+import com.sailpoint.annotation.common.Argument;
+import com.sailpoint.annotation.common.ArgumentsContainer;
+import com.sailpoint.improved.rule.AbstractJavaRuleExecutor;
+import com.sailpoint.improved.rule.util.JavaRuleExecutorUtil;
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import sailpoint.object.Application;
+import sailpoint.object.Attributes;
+import sailpoint.object.JavaRuleContext;
+import sailpoint.object.Rule;
+import sailpoint.object.Schema;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Used with applications of type RuleBasedFileParser, which is used to parse non-delimited
+ * files. This connector can read account and group data from non-standard or free format text. The rule is called
+ * to retrieve each complete record from the file; logic in the rule determines what constitutes a complete record –
+ * whether that is on one line in the file or whether it spans multiple lines.
+ * <p>
+ * NOTE: Since the FileParsingRule rule runs to extract every account or group record from the file and build it into
+ * a map, any time-intensive operations performed in this rule can have a negative performance impact.
+ */
+@Slf4j
+public abstract class FileParsingRule
+        extends AbstractJavaRuleExecutor<Map<String, Object>, FileParsingRule.FileParsingRuleArguments> {
+
+    /**
+     * Name of application argument name
+     */
+    public static final String ARG_APPLICATION_NAME = "application";
+    /**
+     * Name of schema argument name
+     */
+    public static final String ARG_SCHEMA_NAME = "schema";
+    /**
+     * Name of config argument name
+     */
+    public static final String ARG_CONFIG_NAME = "config";
+    /**
+     * Name of inputStream argument name
+     */
+    public static final String ARG_INPUT_STREAM_NAME = "inputStream";
+    /**
+     * Name of reader argument name
+     */
+    public static final String ARG_READER_NAME = "reader";
+    /**
+     * Name of state argument name
+     */
+    public static final String ARG_STATE_NAME = "state";
+    /**
+     * None nulls arguments
+     */
+    public static final List<String> NONE_NULL_ARGUMENTS_NAME = Arrays.asList(
+            FileParsingRule.ARG_APPLICATION_NAME,
+            FileParsingRule.ARG_SCHEMA_NAME,
+            FileParsingRule.ARG_CONFIG_NAME,
+            FileParsingRule.ARG_INPUT_STREAM_NAME,
+            FileParsingRule.ARG_READER_NAME,
+            FileParsingRule.ARG_STATE_NAME
+    );
+
+    /**
+     * Default constructor
+     */
+    public FileParsingRule() {
+        super(Rule.Type.FileParsingRule.name(), NONE_NULL_ARGUMENTS_NAME);
+    }
+
+    /**
+     * Build arguments container for pre-iterator rule
+     *
+     * @param javaRuleContext - current rule context for arguments parameters
+     * @return argument container instance
+     */
+    @Override
+    protected FileParsingRule.FileParsingRuleArguments buildContainerArguments(JavaRuleContext javaRuleContext) {
+        return FileParsingRuleArguments
+                .builder()
+                .application((Application) JavaRuleExecutorUtil.
+                        getArgumentValueByName(javaRuleContext, FileParsingRule.ARG_APPLICATION_NAME))
+                .schema((Schema) JavaRuleExecutorUtil.
+                        getArgumentValueByName(javaRuleContext, FileParsingRule.ARG_SCHEMA_NAME))
+                .config((Attributes) JavaRuleExecutorUtil.
+                        getArgumentValueByName(javaRuleContext, FileParsingRule.ARG_CONFIG_NAME))
+                .inputStream((BufferedInputStream) JavaRuleExecutorUtil.
+                        getArgumentValueByName(javaRuleContext, FileParsingRule.ARG_INPUT_STREAM_NAME))
+                .reader((BufferedReader) JavaRuleExecutorUtil.
+                        getArgumentValueByName(javaRuleContext, FileParsingRule.ARG_READER_NAME))
+                .state((Map<String, Object>) JavaRuleExecutorUtil.
+                        getArgumentValueByName(javaRuleContext, FileParsingRule.ARG_STATE_NAME))
+                .build();
+    }
+
+    /**
+     * Arguments container for {@link FileParsingRule}. Contains:
+     * - application
+     * - schema
+     * - config
+     * - inputStream
+     * - reader
+     * - state
+     */
+    @Data
+    @Builder
+    @ArgumentsContainer
+    public static class FileParsingRuleArguments {
+        /**
+         * A reference to the Application object
+         */
+        @Argument(name = FileParsingRule.ARG_APPLICATION_NAME)
+        private final Application application;
+        /**
+         * A reference to the Schema object for the delimited file source being read
+         */
+        @Argument(name = FileParsingRule.ARG_SCHEMA_NAME)
+        private final Schema schema;
+        /**
+         * Attributes Map of Application configuration attributes
+         */
+        @Argument(name = FileParsingRule.ARG_CONFIG_NAME)
+        private final Attributes config;
+        /**
+         * A reference to the file input stream
+         */
+        @Argument(name = FileParsingRule.ARG_INPUT_STREAM_NAME)
+        private final BufferedInputStream inputStream;
+        /**
+         * A reader wrapping the inputStream
+         */
+        @Argument(name = FileParsingRule.ARG_READER_NAME)
+        private final BufferedReader reader;
+        /**
+         * A Map that can be used to store and share data between executions of this rule during a single aggregation run
+         */
+        @Argument(name = FileParsingRule.ARG_STATE_NAME)
+        private final Map<String, Object> state;
+    }
+}
---
+++ sail-point-improved/src/test/java/com/sailpoint/improved/rule/FileParsingRuleTest.java
@@ -0,0 +1,158 @@
+package com.sailpoint.improved.rule;
+
+import com.sailpoint.improved.rule.connector.FileParsingRule;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import sailpoint.api.SailPointContext;
+import sailpoint.api.SailPointFactory;
+import sailpoint.object.Application;
+import sailpoint.object.Attributes;
+import sailpoint.object.JavaRuleContext;
+import sailpoint.object.Rule;
+import sailpoint.object.Schema;
+import sailpoint.tools.GeneralException;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static com.sailpoint.improved.JUnit4Helper.assertThrows;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.CALLS_REAL_METHODS;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Test for {@link FileParsingRule} class
+ */
+public class FileParsingRuleTest {
+
+    /**
+     * Test instance of {@link FileParsingRule}
+     */
+    private FileParsingRule testRule;
+
+    /**
+     * Mock of {@link SailPointContext} for returning from {@link SailPointFactory#getCurrentContext()}
+     */
+    private SailPointContext sailPointContext;
+
+    /**
+     * Init {@link FileParsingRuleTest#testRule} and {@link FileParsingRuleTest#sailPointContext} for test
+     */
+    @Before
+    public void init() {
+        this.sailPointContext = mock(SailPointContext.class);
+        this.testRule = mock(FileParsingRule.class,
+                Mockito.withSettings().useConstructor().defaultAnswer(CALLS_REAL_METHODS));
+    }
+
+    /**
+     * Test of normal execution
+     * Input:
+     * - valid rule context
+     * Output:
+     * - test map of execution
+     * Expectation:
+     * - application as in rule context args by name {@link FileParsingRule#ARG_APPLICATION_NAME}
+     * - schema as in rule context args by name {@link FileParsingRule#ARG_SCHEMA_NAME}
+     * - config as in rule context args by name {@link FileParsingRule#ARG_CONFIG_NAME}
+     * - inputStream as in rule context args by name {@link FileParsingRule#ARG_INPUT_STREAM_NAME}
+     * - reader as in rule context args by name {@link FileParsingRule#ARG_READER_NAME}
+     * - state as in rule context args by name {@link FileParsingRule#ARG_STATE_NAME}
+     */
+    @Test
+    public void normalTest() throws GeneralException {
+        JavaRuleContext testRuleContext = buildTestJavaRuleContext();
+        Map<String, Object> testResult = Collections.singletonMap(UUID.randomUUID().toString(), UUID.randomUUID());
+
+        doAnswer(invocation -> {
+            assertEquals("SailPoint context is not match", testRuleContext.getContext(), invocation.getArguments()[0]);
+            FileParsingRule.FileParsingRuleArguments arguments = (FileParsingRule.FileParsingRuleArguments) invocation
+                    .getArguments()[1];
+            assertEquals("Application is not match",
+                    testRuleContext.getArguments().get(FileParsingRule.ARG_APPLICATION_NAME),
+                    arguments.getApplication());
+            assertEquals("Schema is not match",
+                    testRuleContext.getArguments().get(FileParsingRule.ARG_SCHEMA_NAME),
+                    arguments.getSchema());
+            assertEquals("Config is not match",
+                    testRuleContext.getArguments().get(FileParsingRule.ARG_CONFIG_NAME),
+                    arguments.getConfig());
+            assertEquals("InputStream is not match",
+                    testRuleContext.getArguments().get(FileParsingRule.ARG_INPUT_STREAM_NAME),
+                    arguments.getInputStream());
+            assertEquals("Reader is not match",
+                    testRuleContext.getArguments().get(FileParsingRule.ARG_READER_NAME),
+                    arguments.getReader());
+            assertEquals("State is not match",
+                    testRuleContext.getArguments().get(FileParsingRule.ARG_STATE_NAME),
+                    arguments.getState());
+            return testResult;
+        }).when(testRule).internalExecute(eq(sailPointContext), any());
+
+        assertEquals(testResult, testRule.execute(testRuleContext));
+        verify(testRule).internalValidation(eq(testRuleContext));
+        verify(testRule).execute(eq(testRuleContext));
+        verify(testRule).internalExecute(eq(sailPointContext), any());
+    }
+
+    /**
+     * Test execution with invalid arguments.
+     * Input:
+     * - invalid rule context: one of none-null arguments is null
+     * Output:
+     * - General exception
+     * Expectation:
+     * - call internalValidation
+     * - do not call internalExecute
+     */
+    @Test
+    public void noneNullValidationFailedTest() throws GeneralException {
+        for (String noneNullArgument : FileParsingRule.NONE_NULL_ARGUMENTS_NAME) {
+            JavaRuleContext testRuleContext = buildTestJavaRuleContext();
+            testRuleContext.getArguments().remove(noneNullArgument);
+
+            assertThrows(GeneralException.class, () -> testRule.execute(testRuleContext));
+            verify(testRule).internalValidation(eq(testRuleContext));
+            verify(testRule, never()).internalExecute(eq(sailPointContext), any());
+        }
+    }
+
+    /**
+     * Test rule type
+     * Input:
+     * - rule type value
+     * Expectation:
+     * - expected rule type
+     */
+    @Test
+    public void ruleTypeTest() {
+        assertEquals("Rule type is not match", Rule.Type.FileParsingRule.name(), testRule.getRuleType());
+    }
+
+    /**
+     * Create valid java rule context for pre-iterate rule
+     *
+     * @return valid rule context
+     */
+    private JavaRuleContext buildTestJavaRuleContext() {
+        Map<String, Object> ruleParameters = new HashMap<>();
+        ruleParameters.put(FileParsingRule.ARG_APPLICATION_NAME, new Application());
+        ruleParameters.put(FileParsingRule.ARG_SCHEMA_NAME, new Schema());
+        ruleParameters.put(FileParsingRule.ARG_CONFIG_NAME, mock(Attributes.class));
+        ruleParameters.put(FileParsingRule.ARG_INPUT_STREAM_NAME, mock(BufferedInputStream.class));
+        ruleParameters.put(FileParsingRule.ARG_READER_NAME, mock(BufferedReader.class));
+        ruleParameters.put(FileParsingRule.ARG_STATE_NAME,
+                Collections.singletonMap(UUID.randomUUID().toString(), UUID.randomUUID()));
+        return new JavaRuleContext(this.sailPointContext, ruleParameters);
+    }
+}
