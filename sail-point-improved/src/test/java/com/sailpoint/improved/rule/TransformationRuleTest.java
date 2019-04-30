1fb53f3a35b5d3da179360a0af2fa905250fdc97
kubakhov <kubakhov.dmitry@gmail.com>
2019-04-30 15:50:43 +0200

    add new rule type -> TransformationRule

---
+++ sail-point-improved/src/test/java/com/sailpoint/improved/rule/TransformationRuleTest.java
@@ -0,0 +1,144 @@
+package com.sailpoint.improved.rule;
+
+import com.sailpoint.improved.rule.connector.TransformationRule;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import sailpoint.api.SailPointContext;
+import sailpoint.api.SailPointFactory;
+import sailpoint.object.Application;
+import sailpoint.object.JavaRuleContext;
+import sailpoint.object.ResourceObject;
+import sailpoint.object.Rule;
+import sailpoint.object.Schema;
+import sailpoint.tools.GeneralException;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static com.sailpoint.improved.JUnit4Helper.assertThrows;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.CALLS_REAL_METHODS;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Test for {@link TransformationRule} class
+ */
+public class TransformationRuleTest {
+
+    /**
+     * Test instance of {@link TransformationRule}
+     */
+    private TransformationRule testRule;
+
+    /**
+     * Mock of {@link SailPointContext} for returning from {@link SailPointFactory#getCurrentContext()}
+     */
+    private SailPointContext sailPointContext;
+
+    /**
+     * Init {@link TransformationRuleTest#testRule} and {@link TransformationRuleTest#sailPointContext} for test
+     */
+    @Before
+    public void init() {
+        this.sailPointContext = mock(SailPointContext.class);
+        this.testRule = mock(TransformationRule.class,
+                Mockito.withSettings().useConstructor().defaultAnswer(CALLS_REAL_METHODS));
+    }
+
+    /**
+     * Test of normal execution
+     * Input:
+     * - valid rule context
+     * Output:
+     * - test map of execution
+     * Expectation:
+     * - application as in rule context args by name {@link TransformationRule#ARG_APPLICATION_NAME}
+     * - schema as in rule context args by name {@link TransformationRule#ARG_SCHEMA_NAME}
+     * - object as in rule context args by name {@link TransformationRule#ARG_OBJECT_NAME}
+     * - context as in sailpoint context in rule context
+     */
+    @Test
+    public void normalTest() throws GeneralException {
+        JavaRuleContext testRuleContext = buildTestJavaRuleContext();
+        ResourceObject testResult = mock(ResourceObject.class);
+
+
+        doAnswer(invocation -> {
+            assertEquals("SailPoint context is not match", testRuleContext.getContext(), invocation.getArguments()[0]);
+            TransformationRule.TransformationRuleArguments arguments = (TransformationRule.TransformationRuleArguments) invocation
+                    .getArguments()[1];
+            assertEquals("Application is not match",
+                    testRuleContext.getArguments().get(TransformationRule.ARG_APPLICATION_NAME),
+                    arguments.getApplication());
+            assertEquals("Schema is not match",
+                    testRuleContext.getArguments().get(TransformationRule.ARG_SCHEMA_NAME),
+                    arguments.getSchema());
+            assertEquals("Object is not match",
+                    testRuleContext.getArguments().get(TransformationRule.ARG_OBJECT_NAME),
+                    arguments.getObject());
+            return testResult;
+        }).when(testRule).internalExecute(eq(sailPointContext), any());
+
+        assertEquals(testResult, testRule.execute(testRuleContext));
+        verify(testRule).internalValidation(eq(testRuleContext));
+        verify(testRule).execute(eq(testRuleContext));
+        verify(testRule).internalExecute(eq(sailPointContext), any());
+    }
+
+    /**
+     * Test execution with invalid arguments.
+     * Input:
+     * - invalid rule context: one of none-null arguments is null
+     * Output:
+     * - General exception
+     * Expectation:
+     * - call internalValidation
+     * - do not call internalExecute
+     */
+    @Test
+    public void noneNullArgumentIsNullTest() throws GeneralException {
+        for (String noneNullArgument : TransformationRule.NONE_NULL_ARGUMENTS_NAME) {
+
+            JavaRuleContext testRuleContext = buildTestJavaRuleContext();
+            testRuleContext.getArguments().remove(noneNullArgument);
+
+            assertThrows(GeneralException.class, () -> testRule.execute(testRuleContext));
+            verify(testRule).internalValidation(eq(testRuleContext));
+            verify(testRule, never()).internalExecute(eq(sailPointContext), any());
+        }
+    }
+
+    /**
+     * Test rule type
+     * Input:
+     * - rule type value
+     * Expectation:
+     * - expected rule type
+     */
+    @Test
+    public void ruleTypeTest() {
+        assertEquals("Rule type is not match", Rule.Type.Transformation.name(), testRule.getRuleType());
+    }
+
+    /**
+     * Create valid java rule context for  build map rule
+     *
+     * @return valid rule context
+     */
+    private JavaRuleContext buildTestJavaRuleContext() {
+        Map<String, Object> ruleParameters = new HashMap<>();
+        ruleParameters.put(TransformationRule.ARG_APPLICATION_NAME, new Application());
+        ruleParameters.put(TransformationRule.ARG_SCHEMA_NAME, new Schema());
+        ruleParameters.put(TransformationRule.ARG_OBJECT_NAME,
+                Collections.singletonMap(UUID.randomUUID().toString(), UUID.randomUUID()));
+        return new JavaRuleContext(this.sailPointContext, ruleParameters);
+    }
+}
