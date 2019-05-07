96b544d34bfbd5500fd62a285dea66a76edeb782
kubakhov <kubakhov.dmitry@gmail.com>
2019-05-07 12:23:45 +0200

    add new rule type -> ResourceObjectCustomization

---
+++ sail-point-improved/src/main/java/com/sailpoint/improved/rule/aggregation/ResourceObjectCustomizationRule.java
@@ -0,0 +1,130 @@
+package com.sailpoint.improved.rule.aggregation;
+
+import com.sailpoint.annotation.common.Argument;
+import com.sailpoint.annotation.common.ArgumentsContainer;
+import com.sailpoint.improved.rule.AbstractJavaRuleExecutor;
+import com.sailpoint.improved.rule.util.JavaRuleExecutorUtil;
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import sailpoint.connector.AbstractConnector;
+import sailpoint.object.Application;
+import sailpoint.object.JavaRuleContext;
+import sailpoint.object.ResourceObject;
+import sailpoint.object.Rule;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Runs prior to any other aggregation rule to customize the resource object
+ * provided by the connector before aggregation begins. Connectors that provide a transformation rule may not
+ * need to use a ResourceObjectCustomization rule, since the transformation rule can modify the ResourceObject
+ * as needed. However, many connectors directly provide a resource object, without the hooks for processing the
+ * data through rules like a transformation rule, so this rule allows customization of the resource object before
+ * IdentityIQ attempts to correlate the object to an Identity.
+ * <p>
+ * NOTE: Since the ResourceObjectCustomization rule runs for every ResourceObject provided by the connector,
+ * time-intensive operations performed in it can have a negative impact on task performance. It runs even when
+ * optimized aggregation has been specified, so customizations made by this rule can impact the optimization
+ * decisions for the ResourceObject.
+ * <p>
+ * Outputs:
+ * Even though the ResourceObject is passed to the rule where it can be modified directly, it must be
+ * returned from the rule for its changes to be used by the rest of the aggregation process. Otherwise, changes
+ * made to it inside the rule are not transferred back to the rest of the process.
+ */
+@Slf4j
+public abstract class ResourceObjectCustomizationRule
+        extends AbstractJavaRuleExecutor<ResourceObject, ResourceObjectCustomizationRule.ResourceObjectCustomizationRuleArguments> {
+
+    /**
+     * Name of object argument name
+     */
+    public static final String ARG_OBJECT_NAME = "object";
+    /**
+     * Name of application argument name
+     */
+    public static final String ARG_APPLICATION_NAME = "application";
+    /**
+     * Name of connector argument name
+     */
+    public static final String ARG_CONNECTOR_NAME = "connector";
+    /**
+     * Name of state argument name
+     */
+    public static final String ARG_STATE_NAME = "state";
+
+    /**
+     * None nulls arguments
+     */
+    public static final List<String> NONE_NULL_ARGUMENTS_NAME = Arrays.asList(
+            ResourceObjectCustomizationRule.ARG_OBJECT_NAME,
+            ResourceObjectCustomizationRule.ARG_APPLICATION_NAME,
+            ResourceObjectCustomizationRule.ARG_CONNECTOR_NAME,
+            ResourceObjectCustomizationRule.ARG_STATE_NAME
+    );
+
+    /**
+     * Default constructor
+     */
+    public ResourceObjectCustomizationRule() {
+        super(Rule.Type.ResourceObjectCustomization.name(), NONE_NULL_ARGUMENTS_NAME);
+    }
+
+    /**
+     * Build arguments container for current rule
+     *
+     * @param javaRuleContext - current rule context
+     * @return argument container instance
+     */
+    @Override
+    protected ResourceObjectCustomizationRule.ResourceObjectCustomizationRuleArguments buildContainerArguments(
+            JavaRuleContext javaRuleContext) {
+        return ResourceObjectCustomizationRuleArguments
+                .builder()
+                .object((ResourceObject) JavaRuleExecutorUtil.
+                        getArgumentValueByName(javaRuleContext, ResourceObjectCustomizationRule.ARG_OBJECT_NAME))
+                .application((Application) JavaRuleExecutorUtil.
+                        getArgumentValueByName(javaRuleContext, ResourceObjectCustomizationRule.ARG_APPLICATION_NAME))
+                .connector((AbstractConnector) JavaRuleExecutorUtil.
+                        getArgumentValueByName(javaRuleContext, ResourceObjectCustomizationRule.ARG_CONNECTOR_NAME))
+                .state((Map<String, Object>) JavaRuleExecutorUtil.
+                        getArgumentValueByName(javaRuleContext, ResourceObjectCustomizationRule.ARG_STATE_NAME))
+                .build();
+    }
+
+    /**
+     * Arguments container for {@link ResourceObjectCustomizationRule}. Contains:
+     * - object
+     * - application
+     * - connector
+     * - state
+     */
+    @Data
+    @Builder
+    @ArgumentsContainer
+    public static class ResourceObjectCustomizationRuleArguments {
+        /**
+         * A reference to the resource object built by the connector
+         */
+        @Argument(name = ResourceObjectCustomizationRule.ARG_OBJECT_NAME)
+        private final ResourceObject object;
+        /**
+         * A reference to the Application object
+         */
+        @Argument(name = ResourceObjectCustomizationRule.ARG_APPLICATION_NAME)
+        private final Application application;
+        /**
+         * A reference to the Connector object used by this application
+         */
+        @Argument(name = ResourceObjectCustomizationRule.ARG_CONNECTOR_NAME)
+        private final AbstractConnector connector;
+        /**
+         * A Map that can be used to store and share data between executions of this rule during a single aggregation run
+         */
+        @Argument(name = ResourceObjectCustomizationRule.ARG_STATE_NAME)
+        private final Map<String, Object> state;
+    }
+}
---
+++ sail-point-improved/src/test/java/com/sailpoint/improved/rule/ResourceObjectCustomizationRuleTest.java
@@ -0,0 +1,146 @@
+package com.sailpoint.improved.rule;
+
+import com.sailpoint.improved.rule.aggregation.ResourceObjectCustomizationRule;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import sailpoint.api.SailPointContext;
+import sailpoint.api.SailPointFactory;
+import sailpoint.connector.AbstractConnector;
+import sailpoint.object.Application;
+import sailpoint.object.JavaRuleContext;
+import sailpoint.object.ResourceObject;
+import sailpoint.object.Rule;
+import sailpoint.tools.GeneralException;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.sailpoint.improved.JUnit4Helper.assertThrows;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.CALLS_REAL_METHODS;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Test for {@link ResourceObjectCustomizationRule} class
+ */
+public class ResourceObjectCustomizationRuleTest {
+
+    /**
+     * Test instance of {@link ResourceObjectCustomizationRule}
+     */
+    private ResourceObjectCustomizationRule testRule;
+
+    /**
+     * Mock of {@link SailPointContext} for returning from {@link SailPointFactory#getCurrentContext()}
+     */
+    private SailPointContext sailPointContext;
+
+    /**
+     * Init {@link ResourceObjectCustomizationRuleTest#testRule} and {@link ResourceObjectCustomizationRuleTest#sailPointContext} for test
+     */
+    @Before
+    public void init() {
+        this.sailPointContext = mock(SailPointContext.class);
+        this.testRule = mock(ResourceObjectCustomizationRule.class,
+                Mockito.withSettings().useConstructor().defaultAnswer(CALLS_REAL_METHODS));
+    }
+
+    /**
+     * Test of normal execution
+     * Input:
+     * - valid rule context
+     * Output:
+     * - test object of execution
+     * Expectation:
+     * - object as in rule context args by name {@link ResourceObjectCustomizationRule#ARG_OBJECT_NAME}
+     * - application as in rule context args by name {@link ResourceObjectCustomizationRule#ARG_APPLICATION_NAME}
+     * - connector as in rule context args by name {@link ResourceObjectCustomizationRule#ARG_CONNECTOR_NAME}
+     * - state as in rule context args by name {@link ResourceObjectCustomizationRule#ARG_STATE_NAME}
+     * - context as in sailpoint context in rule context
+     */
+    @Test
+    public void normalTest() throws GeneralException {
+        JavaRuleContext testRuleContext = buildTestJavaRuleContext();
+        ResourceObject testResult = mock(ResourceObject.class);
+
+        doAnswer(invocation -> {
+            assertEquals("SailPoint context is not match", testRuleContext.getContext(), invocation.getArguments()[0]);
+            ResourceObjectCustomizationRule.ResourceObjectCustomizationRuleArguments arguments = (ResourceObjectCustomizationRule.ResourceObjectCustomizationRuleArguments) invocation
+                    .getArguments()[1];
+            assertEquals("Object is not match",
+                    testRuleContext.getArguments().get(ResourceObjectCustomizationRule.ARG_OBJECT_NAME),
+                    arguments.getObject());
+            assertEquals("Application is not match",
+                    testRuleContext.getArguments().get(ResourceObjectCustomizationRule.ARG_APPLICATION_NAME),
+                    arguments.getApplication());
+            assertEquals("Connector is not match",
+                    testRuleContext.getArguments().get(ResourceObjectCustomizationRule.ARG_CONNECTOR_NAME),
+                    arguments.getConnector());
+            assertEquals("State is not match",
+                    testRuleContext.getArguments().get(ResourceObjectCustomizationRule.ARG_STATE_NAME),
+                    arguments.getState());
+            return testResult;
+        }).when(testRule).internalExecute(eq(sailPointContext), any());
+
+        assertEquals(testResult, testRule.execute(testRuleContext));
+        verify(testRule).internalValidation(eq(testRuleContext));
+        verify(testRule).execute(eq(testRuleContext));
+        verify(testRule).internalExecute(eq(sailPointContext), any());
+    }
+
+    /**
+     * Test execution with invalid arguments.
+     * Input:
+     * - invalid rule context: one of none-null arguments is null
+     * Output:
+     * - General exception
+     * Expectation:
+     * - call internalValidation
+     * - do not call internalExecute
+     */
+    @Test
+    public void noneNullArgumentIsNullTest() throws GeneralException {
+        for (String noneNullArgument : ResourceObjectCustomizationRule.NONE_NULL_ARGUMENTS_NAME) {
+
+            JavaRuleContext testRuleContext = buildTestJavaRuleContext();
+            testRuleContext.getArguments().remove(noneNullArgument);
+
+            assertThrows(GeneralException.class, () -> testRule.execute(testRuleContext));
+            verify(testRule).internalValidation(eq(testRuleContext));
+            verify(testRule, never()).internalExecute(eq(sailPointContext), any());
+        }
+    }
+
+    /**
+     * Test rule type
+     * Input:
+     * - rule type value
+     * Expectation:
+     * - expected rule type
+     */
+    @Test
+    public void ruleTypeTest() {
+        assertEquals("Rule type is not match", Rule.Type.ResourceObjectCustomization.name(), testRule.getRuleType());
+    }
+
+    /**
+     * Create valid java rule context for current rule
+     *
+     * @return valid rule context
+     */
+    private JavaRuleContext buildTestJavaRuleContext() {
+        Map<String, Object> ruleParameters = new HashMap<>();
+        ruleParameters.put(ResourceObjectCustomizationRule.ARG_OBJECT_NAME, mock(ResourceObject.class));
+        ruleParameters.put(ResourceObjectCustomizationRule.ARG_APPLICATION_NAME, new Application());
+        ruleParameters.put(ResourceObjectCustomizationRule.ARG_CONNECTOR_NAME, mock(AbstractConnector.class));
+        ruleParameters.put(ResourceObjectCustomizationRule.ARG_STATE_NAME, Collections.emptyMap());
+        return new JavaRuleContext(this.sailPointContext, ruleParameters);
+    }
+}
