0a87c6cbd0bf53a9a88171a39871b64de188d5bf
kubakhov <kubakhov.dmitry@gmail.com>
2019-05-08 10:59:24 +0200

    add new rule type -> Certifier

---
+++ sail-point-improved/src/main/java/com/sailpoint/improved/rule/certification/CertifierRule.java
@@ -0,0 +1,117 @@
+package com.sailpoint.improved.rule.certification;
+
+import com.sailpoint.annotation.common.Argument;
+import com.sailpoint.annotation.common.ArgumentsContainer;
+import com.sailpoint.improved.rule.AbstractJavaRuleExecutor;
+import com.sailpoint.improved.rule.util.JavaRuleExecutorUtil;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import sailpoint.object.GroupDefinition;
+import sailpoint.object.GroupFactory;
+import sailpoint.object.Identity;
+import sailpoint.object.JavaRuleContext;
+import sailpoint.object.Rule;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Used only with Advanced certifications that are certifying members of GroupFactory-
+ * generated Groups. It identifies the certifier for each Group. This rule runs once for each Group generated from
+ * the specified GroupFactory; if the certification includes more than one GroupFactory, a separate rule can be
+ * specified for each GroupFactory
+ * <p>
+ * Output:
+ * Identifies the Certifier(s) for each group created from the groupFactory; can return
+ * - an identity name, a CSV list of identity names
+ * - an Identity object, a List of Identity objects
+ * - a List of Identity names
+ *
+ * <T> - return type of rule:
+ * - {@link String}
+ * - {@link Identity}
+ * - {@link List<String>}
+ * - {@link List<Identity>}
+ */
+@Slf4j
+public abstract class CertifierRule<T extends Object>
+        extends AbstractJavaRuleExecutor<T, CertifierRule.CertifierRuleArguments> {
+
+    /**
+     * Name of factory argument name
+     */
+    public static final String ARG_FACTORY_NAME = "factory";
+    /**
+     * Name of group argument name
+     */
+    public static final String ARG_GROUP_NAME = "group";
+    /**
+     * Name of state argument name
+     */
+    public static final String ARG_STATE_NAME = "state";
+
+    /**
+     * None nulls arguments
+     */
+    public static final List<String> NONE_NULL_ARGUMENTS_NAME = Arrays.asList(
+            CertifierRule.ARG_FACTORY_NAME,
+            CertifierRule.ARG_GROUP_NAME,
+            CertifierRule.ARG_STATE_NAME
+    );
+
+    /**
+     * Default constructor
+     */
+    public CertifierRule() {
+        super(Rule.Type.Certifier.name(), CertifierRule.NONE_NULL_ARGUMENTS_NAME);
+    }
+
+    /**
+     * Build arguments container for current rule
+     *
+     * @param javaRuleContext - current rule context
+     * @return argument container instance
+     */
+    @Override
+    protected CertifierRule.CertifierRuleArguments buildContainerArguments(
+            @NonNull JavaRuleContext javaRuleContext) {
+        return CertifierRuleArguments.builder()
+                .factory((GroupFactory) JavaRuleExecutorUtil
+                        .getArgumentValueByName(javaRuleContext, CertifierRule.ARG_FACTORY_NAME))
+                .group((GroupDefinition) JavaRuleExecutorUtil
+                        .getArgumentValueByName(javaRuleContext, CertifierRule.ARG_GROUP_NAME))
+                .state((Map<String, Object>) JavaRuleExecutorUtil
+                        .getArgumentValueByName(javaRuleContext, CertifierRule.ARG_STATE_NAME))
+                .build();
+    }
+
+    /**
+     * Arguments container for current rule. Contains:
+     * - factory
+     * - group
+     * - state
+     */
+    @Data
+    @Builder
+    @ArgumentsContainer
+    public static class CertifierRuleArguments {
+        /**
+         * The groupFactory object that created the group(s) being certified
+         */
+        @Argument(name = CertifierRule.ARG_FACTORY_NAME)
+        private final GroupFactory factory;
+        /**
+         * The group object whose members are being assigned a certifier by the rule
+         */
+        @Argument(name = CertifierRule.ARG_GROUP_NAME)
+        private final GroupDefinition group;
+        /**
+         * Map in which any data can be stored; shared across multiple rules in the certification generation process
+         */
+        @Argument(name = CertifierRule.ARG_STATE_NAME)
+        private final Map<String, Object> state;
+    }
+}
---
+++ sail-point-improved/src/test/java/com/sailpoint/improved/rule/CertifierRuleTest.java
@@ -0,0 +1,261 @@
+package com.sailpoint.improved.rule;
+
+import com.sailpoint.improved.rule.certification.CertifierRule;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import sailpoint.api.SailPointContext;
+import sailpoint.api.SailPointFactory;
+import sailpoint.object.GroupDefinition;
+import sailpoint.object.GroupFactory;
+import sailpoint.object.Identity;
+import sailpoint.object.JavaRuleContext;
+import sailpoint.object.Rule;
+import sailpoint.tools.GeneralException;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import static com.sailpoint.improved.JUnit4Helper.assertThrows;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.CALLS_REAL_METHODS;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Test for {@link CertifierRule} class
+ */
+public class CertifierRuleTest {
+
+    /**
+     * Test instance of {@link CertifierRule}
+     */
+    private CertifierRule testRule;
+
+    /**
+     * Mock of {@link SailPointContext} for returning from {@link SailPointFactory#getCurrentContext()}
+     */
+    private SailPointContext sailPointContext;
+
+    /**
+     * Init {@link CertifierRuleTest#testRule} and {@link CertifierRuleTest#sailPointContext} for test
+     */
+    @Before
+    public void init() {
+        this.sailPointContext = mock(SailPointContext.class);
+        this.testRule = mock(CertifierRule.class,
+                Mockito.withSettings().useConstructor().defaultAnswer(CALLS_REAL_METHODS));
+    }
+
+    /**
+     * Test of normal execution with {@link String} as result
+     * Input:
+     * - valid rule context
+     * Output:
+     * - test string value
+     * Expectation:
+     * - factory as in rule context args by name {@link CertifierRule#ARG_FACTORY_NAME}
+     * - group as in rule context args by name {@link CertifierRule#ARG_GROUP_NAME}
+     * - state as in rule context args by name {@link CertifierRule#ARG_STATE_NAME}
+     * - context as in sailpoint context in rule context
+     */
+    @Test
+    public void normalStringReturnTypeTest() throws GeneralException {
+        JavaRuleContext testRuleContext = buildTestJavaRuleContext();
+        String testResult = UUID.randomUUID().toString();
+
+        doAnswer(invocation -> {
+            assertEquals("SailPoint context is not match", testRuleContext.getContext(), invocation.getArguments()[0]);
+            CertifierRule.CertifierRuleArguments arguments = (CertifierRule.CertifierRuleArguments) invocation
+                    .getArguments()[1];
+            assertEquals("Factory is not match",
+                    testRuleContext.getArguments().get(CertifierRule.ARG_FACTORY_NAME),
+                    arguments.getFactory());
+            assertEquals("Group is not match",
+                    testRuleContext.getArguments().get(CertifierRule.ARG_GROUP_NAME),
+                    arguments.getGroup());
+            assertEquals("State is not match",
+                    testRuleContext.getArguments().get(CertifierRule.ARG_STATE_NAME),
+                    arguments.getState());
+            return testResult;
+        }).when(testRule).internalExecute(eq(sailPointContext), any());
+
+        assertEquals(testResult, testRule.execute(testRuleContext));
+        verify(testRule).internalValidation(eq(testRuleContext));
+        verify(testRule).execute(eq(testRuleContext));
+        verify(testRule).internalExecute(eq(sailPointContext), any());
+    }
+
+    /**
+     * Test of normal execution with {@link Identity} as result
+     * Input:
+     * - valid rule context
+     * Output:
+     * - test identity value
+     * Expectation:
+     * - factory as in rule context args by name {@link CertifierRule#ARG_FACTORY_NAME}
+     * - group as in rule context args by name {@link CertifierRule#ARG_GROUP_NAME}
+     * - state as in rule context args by name {@link CertifierRule#ARG_STATE_NAME}
+     * - context as in sailpoint context in rule context
+     */
+    @Test
+    public void normalIdentityReturnTypeTest() throws GeneralException {
+        JavaRuleContext testRuleContext = buildTestJavaRuleContext();
+        Identity testResult = mock(Identity.class);
+
+        doAnswer(invocation -> {
+            assertEquals("SailPoint context is not match", testRuleContext.getContext(), invocation.getArguments()[0]);
+            CertifierRule.CertifierRuleArguments arguments = (CertifierRule.CertifierRuleArguments) invocation
+                    .getArguments()[1];
+            assertEquals("Factory is not match",
+                    testRuleContext.getArguments().get(CertifierRule.ARG_FACTORY_NAME),
+                    arguments.getFactory());
+            assertEquals("Group is not match",
+                    testRuleContext.getArguments().get(CertifierRule.ARG_GROUP_NAME),
+                    arguments.getGroup());
+            assertEquals("State is not match",
+                    testRuleContext.getArguments().get(CertifierRule.ARG_STATE_NAME),
+                    arguments.getState());
+            return testResult;
+        }).when(testRule).internalExecute(eq(sailPointContext), any());
+
+        assertEquals(testResult, testRule.execute(testRuleContext));
+        verify(testRule).internalValidation(eq(testRuleContext));
+        verify(testRule).execute(eq(testRuleContext));
+        verify(testRule).internalExecute(eq(sailPointContext), any());
+    }
+
+    /**
+     * Test of normal execution with {@link List<String>} as result
+     * Input:
+     * - valid rule context
+     * Output:
+     * - test list of string value
+     * Expectation:
+     * - factory as in rule context args by name {@link CertifierRule#ARG_FACTORY_NAME}
+     * - group as in rule context args by name {@link CertifierRule#ARG_GROUP_NAME}
+     * - state as in rule context args by name {@link CertifierRule#ARG_STATE_NAME}
+     * - context as in sailpoint context in rule context
+     */
+    @Test
+    public void normalStringListReturnTypeTest() throws GeneralException {
+        JavaRuleContext testRuleContext = buildTestJavaRuleContext();
+        List<String> testResult = Collections.singletonList(UUID.randomUUID().toString());
+
+        doAnswer(invocation -> {
+            assertEquals("SailPoint context is not match", testRuleContext.getContext(), invocation.getArguments()[0]);
+            CertifierRule.CertifierRuleArguments arguments = (CertifierRule.CertifierRuleArguments) invocation
+                    .getArguments()[1];
+            assertEquals("Factory is not match",
+                    testRuleContext.getArguments().get(CertifierRule.ARG_FACTORY_NAME),
+                    arguments.getFactory());
+            assertEquals("Group is not match",
+                    testRuleContext.getArguments().get(CertifierRule.ARG_GROUP_NAME),
+                    arguments.getGroup());
+            assertEquals("State is not match",
+                    testRuleContext.getArguments().get(CertifierRule.ARG_STATE_NAME),
+                    arguments.getState());
+            return testResult;
+        }).when(testRule).internalExecute(eq(sailPointContext), any());
+
+        assertEquals(testResult, testRule.execute(testRuleContext));
+        verify(testRule).internalValidation(eq(testRuleContext));
+        verify(testRule).execute(eq(testRuleContext));
+        verify(testRule).internalExecute(eq(sailPointContext), any());
+    }
+
+    /**
+     * Test of normal execution with {@link List<Identity>} as result
+     * Input:
+     * - valid rule context
+     * Output:
+     * - list of test identity value
+     * Expectation:
+     * - factory as in rule context args by name {@link CertifierRule#ARG_FACTORY_NAME}
+     * - group as in rule context args by name {@link CertifierRule#ARG_GROUP_NAME}
+     * - state as in rule context args by name {@link CertifierRule#ARG_STATE_NAME}
+     * - context as in sailpoint context in rule context
+     */
+    @Test
+    public void normalIdentityListReturnTypeTest() throws GeneralException {
+        JavaRuleContext testRuleContext = buildTestJavaRuleContext();
+        List<Identity> testResult = Collections.singletonList(mock(Identity.class));
+
+        doAnswer(invocation -> {
+            assertEquals("SailPoint context is not match", testRuleContext.getContext(), invocation.getArguments()[0]);
+            CertifierRule.CertifierRuleArguments arguments = (CertifierRule.CertifierRuleArguments) invocation
+                    .getArguments()[1];
+            assertEquals("Factory is not match",
+                    testRuleContext.getArguments().get(CertifierRule.ARG_FACTORY_NAME),
+                    arguments.getFactory());
+            assertEquals("Group is not match",
+                    testRuleContext.getArguments().get(CertifierRule.ARG_GROUP_NAME),
+                    arguments.getGroup());
+            assertEquals("State is not match",
+                    testRuleContext.getArguments().get(CertifierRule.ARG_STATE_NAME),
+                    arguments.getState());
+            return testResult;
+        }).when(testRule).internalExecute(eq(sailPointContext), any());
+
+        assertEquals(testResult, testRule.execute(testRuleContext));
+        verify(testRule).internalValidation(eq(testRuleContext));
+        verify(testRule).execute(eq(testRuleContext));
+        verify(testRule).internalExecute(eq(sailPointContext), any());
+    }
+
+    /**
+     * Test execution with invalid arguments.
+     * Input:
+     * - invalid rule context: one of none-null arguments is null
+     * Output:
+     * - General exception
+     * Expectation:
+     * - call internalValidation
+     * - do not call internalExecute
+     */
+    @Test
+    public void noneNullArgumentIsNullTest() throws GeneralException {
+        for (String noneNullArgument : CertifierRule.NONE_NULL_ARGUMENTS_NAME) {
+
+            JavaRuleContext testRuleContext = buildTestJavaRuleContext();
+            testRuleContext.getArguments().remove(noneNullArgument);
+
+            assertThrows(GeneralException.class, () -> testRule.execute(testRuleContext));
+            verify(testRule).internalValidation(eq(testRuleContext));
+            verify(testRule, never()).internalExecute(eq(sailPointContext), any());
+        }
+    }
+
+    /**
+     * Test rule type
+     * Input:
+     * - rule type value
+     * Expectation:
+     * - expected rule type
+     */
+    @Test
+    public void ruleTypeTest() {
+        assertEquals("Rule type is not match", Rule.Type.Certifier.name(), testRule.getRuleType());
+    }
+
+    /**
+     * Create valid java rule context for current rule
+     *
+     * @return valid rule context
+     */
+    private JavaRuleContext buildTestJavaRuleContext() {
+        Map<String, Object> ruleParameters = new HashMap<>();
+        ruleParameters.put(CertifierRule.ARG_FACTORY_NAME, mock(GroupFactory.class));
+        ruleParameters.put(CertifierRule.ARG_GROUP_NAME, mock(GroupDefinition.class));
+        ruleParameters.put(CertifierRule.ARG_STATE_NAME,
+                Collections.singletonMap(UUID.randomUUID().toString(), UUID.randomUUID()));
+        return new JavaRuleContext(this.sailPointContext, ruleParameters);
+    }
+}
