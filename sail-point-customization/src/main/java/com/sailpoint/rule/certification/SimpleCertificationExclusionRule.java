da7d737a33aef2a4f448022e3e976b584f2d8650
kubakhov <kubakhov.dmitry@gmail.com>
2019-05-08 09:23:24 +0200

    add simple implementation of CertificationExclusion rule type

---
+++ sail-point-customization/src/main/java/com/sailpoint/rule/certification/SimpleCertificationExclusionRule.java
@@ -0,0 +1,30 @@
+package com.sailpoint.rule.certification;
+
+import com.sailpoint.annotation.Rule;
+import com.sailpoint.annotation.common.Argument;
+import com.sailpoint.annotation.common.ArgumentType;
+import com.sailpoint.improved.rule.certification.CertificationExclusionRule;
+import lombok.extern.slf4j.Slf4j;
+import sailpoint.api.SailPointContext;
+
+import java.util.UUID;
+
+/**
+ * Simple implementation of {@link CertificationExclusionRule} rule
+ */
+@Slf4j
+@Rule(value = "Simple certification exclusion rule", type = sailpoint.object.Rule.Type.CertificationExclusion)
+public class SimpleCertificationExclusionRule extends CertificationExclusionRule {
+
+    /**
+     * Log current certification by INFO level and return random string
+     */
+    @Override
+    @Argument(name = "explanation", type = ArgumentType.RETURNS, isReturnsType = true)
+    protected String internalExecute(SailPointContext context,
+                                     CertificationExclusionRule.CertificationExclusionRuleArguments arguments) {
+
+        log.info("Certification:[{}]", arguments.getCertification());
+        return UUID.randomUUID().toString();
+    }
+}
