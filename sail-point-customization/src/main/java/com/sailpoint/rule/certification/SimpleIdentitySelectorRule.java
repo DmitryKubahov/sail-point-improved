50736604e125bfca41e0efd157bc3fdb01ff1453
kubakhov <kubakhov.dmitry@gmail.com>
2019-05-08 14:30:54 +0200

    add simple implementation of IdentitySelection rule type

---
+++ sail-point-customization/src/main/java/com/sailpoint/rule/certification/SimpleIdentitySelectorRule.java
@@ -0,0 +1,28 @@
+package com.sailpoint.rule.certification;
+
+import com.sailpoint.annotation.Rule;
+import com.sailpoint.annotation.common.Argument;
+import com.sailpoint.annotation.common.ArgumentType;
+import com.sailpoint.improved.rule.certification.IdentitySelectorRule;
+import lombok.extern.slf4j.Slf4j;
+import sailpoint.api.SailPointContext;
+
+/**
+ * Simple implementation of {@link IdentitySelectorRule} rule
+ */
+@Slf4j
+@Rule(value = "Simple identity selector rule", type = sailpoint.object.Rule.Type.IdentitySelector)
+public class SimpleIdentitySelectorRule extends IdentitySelectorRule {
+
+    /**
+     * Log current identity and return true
+     */
+    @Override
+    @Argument(name = "success", type = ArgumentType.RETURNS, isReturnsType = true)
+    protected Boolean internalExecute(SailPointContext context,
+                                      IdentitySelectorRuleArguments arguments) {
+
+        log.info("Identity:[{}]", arguments.getIdentity());
+        return Boolean.TRUE;
+    }
+}
