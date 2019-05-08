338559934e70a06843b489a848e23794e30eaaec
kubakhov <kubakhov.dmitry@gmail.com>
2019-05-08 14:30:22 +0200

    add new rule type -> IdentitySelector

---
+++ sail-point-improved/src/test/java/com/sailpoint/improved/rule/IdentitySelectorRuleTest.java
@@ -0,0 +1,132 @@
+package com.sailpoint.improved.rule;
+
+import com.sailpoint.improved.rule.certification.IdentitySelectorRule;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import sailpoint.api.SailPointContext;
+import sailpoint.api.SailPointFactory;
+import sailpoint.object.Identity;
+import sailpoint.object.JavaRuleContext;
+import sailpoint.object.Rule;
+import sailpoint.tools.GeneralException;
+import sailpoint.tools.Message;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static com.sailpoint.improved.JUnit4Helper.assertThrows;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.CALLS_REAL_METHODS;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Test for {@link IdentitySelectorRule} class
+ */
+public class IdentitySelectorRuleTest {
+
+    /**
+     * Test instance of {@link IdentitySelectorRule}
+     */
+    private IdentitySelectorRule testRule;
+
+    /**
+     * Mock of {@link SailPointContext} for returning from {@link SailPointFactory#getCurrentContext()}
+     */
+    private SailPointContext sailPointContext;
+
+    /**
+     * Init {@link IdentitySelectorRuleTest#testRule} and {@link IdentitySelectorRuleTest#sailPointContext} for test
+     */
+    @Before
+    public void init() {
+        this.sailPointContext = mock(SailPointContext.class);
+        this.testRule = mock(IdentitySelectorRule.class,
+                Mockito.withSettings().useConstructor().defaultAnswer(CALLS_REAL_METHODS));
+    }
+
+    /**
+     * Test of normal execution with return list type of {@link Message}
+     * Input:
+     * - valid rule context
+     * Output:
+     * - test list of test messages value
+     * Expectation:
+     * - identity as in rule context args by name {@link IdentitySelectorRule#ARG_IDENTITY_NAME}
+     * - context as in sailpoint context in rule context
+     */
+    @Test
+    public void normalExecutionTest() throws GeneralException {
+        JavaRuleContext testRuleContext = buildTestJavaRuleContext();
+        Boolean testResult = Boolean.TRUE;
+
+        doAnswer(invocation -> {
+            assertEquals("SailPoint context is not match", testRuleContext.getContext(), invocation.getArguments()[0]);
+            IdentitySelectorRule.IdentitySelectorRuleArguments arguments = (IdentitySelectorRule.IdentitySelectorRuleArguments) invocation
+                    .getArguments()[1];
+            assertEquals("Identity is not match",
+                    testRuleContext.getArguments().get(IdentitySelectorRule.ARG_IDENTITY_NAME),
+                    arguments.getIdentity());
+            return testResult;
+        }).when(testRule).internalExecute(eq(sailPointContext), any());
+
+        assertEquals(testResult, testRule.execute(testRuleContext));
+        verify(testRule).internalValidation(eq(testRuleContext));
+        verify(testRule).execute(eq(testRuleContext));
+        verify(testRule).internalExecute(eq(sailPointContext), any());
+    }
+
+    /**
+     * Test execution with invalid arguments.
+     * Input:
+     * - invalid rule context: one of none-null arguments is null
+     * Output:
+     * - General exception
+     * Expectation:
+     * - call internalValidation
+     * - do not call internalExecute
+     */
+    @Test
+    public void noneNullArgumentIsNullTest() throws GeneralException {
+        for (String noneNullArgument : IdentitySelectorRule.NONE_NULL_ARGUMENTS_NAME) {
+
+            JavaRuleContext testRuleContext = buildTestJavaRuleContext();
+            testRuleContext.getArguments().remove(noneNullArgument);
+
+            assertThrows(GeneralException.class, () -> testRule.execute(testRuleContext));
+            verify(testRule).internalValidation(eq(testRuleContext));
+            verify(testRule, never()).internalExecute(eq(sailPointContext), any());
+        }
+    }
+
+    /**
+     * Test rule type
+     * Input:
+     * - rule type value
+     * Expectation:
+     * - expected rule type
+     */
+    @Test
+    public void ruleTypeTest() {
+        assertEquals("Rule type is not match", Rule.Type.IdentitySelector.name(),
+                testRule.getRuleType());
+    }
+
+    /**
+     * Create valid java rule context for current rule
+     *
+     * @return valid rule context
+     */
+    private JavaRuleContext buildTestJavaRuleContext() {
+        Map<String, Object> ruleParameters = new HashMap<>();
+        ruleParameters.put(IdentitySelectorRule.ARG_IDENTITY_NAME, mock(Identity.class));
+        return new JavaRuleContext(this.sailPointContext, ruleParameters);
+    }
+}
