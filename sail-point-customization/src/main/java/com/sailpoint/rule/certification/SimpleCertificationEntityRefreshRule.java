2b30ac30490e18c7d4364172e090d46182e4685d
kubakhov <kubakhov.dmitry@gmail.com>
2019-05-08 12:21:13 +0200

    add simple implementation of CertificationEntityRefresh rule type

---
+++ sail-point-customization/src/main/java/com/sailpoint/rule/certification/SimpleCertificationEntityRefreshRule.java
@@ -0,0 +1,24 @@
+package com.sailpoint.rule.certification;
+
+import com.sailpoint.annotation.Rule;
+import com.sailpoint.improved.rule.certification.CertificationEntityRefreshRule;
+import lombok.extern.slf4j.Slf4j;
+import sailpoint.api.SailPointContext;
+
+/**
+ * Simple implementation of {@link CertificationEntityRefreshRule} rule
+ */
+@Slf4j
+@Rule(value = "Simple certification entity refresh rule", type = sailpoint.object.Rule.Type.CertificationEntityRefresh)
+public class SimpleCertificationEntityRefreshRule extends CertificationEntityRefreshRule {
+
+    /**
+     * Log current certification entity by INFO and return it
+     */
+    @Override
+    protected Object internalExecute(SailPointContext context, CertificationEntityRefreshRuleArguments arguments) {
+
+        log.info("Certification entity:[{}]", arguments.getCertificationEntity());
+        return arguments.getCertificationEntity();
+    }
+}
