0ee3dace3380216d33a0230aac4cb0845ce73993
kubakhov <kubakhov.dmitry@gmail.com>
2019-05-08 10:37:47 +0200

    add new rule type -> CertificationPreDelegation

---
+++ sail-point-improved/src/main/java/com/sailpoint/improved/rule/certification/CertificationPreDelegationRule.java
@@ -0,0 +1,249 @@
+package com.sailpoint.improved.rule.certification;
+
+import com.sailpoint.annotation.common.Argument;
+import com.sailpoint.annotation.common.ArgumentsContainer;
+import com.sailpoint.improved.rule.AbstractJavaRuleExecutor;
+import com.sailpoint.improved.rule.connector.FileParsingRule;
+import com.sailpoint.improved.rule.util.JavaRuleExecutorUtil;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import sailpoint.api.CertificationContext;
+import sailpoint.object.Certification;
+import sailpoint.object.CertificationEntity;
+import sailpoint.object.Identity;
+import sailpoint.object.JavaRuleContext;
+import sailpoint.object.Rule;
+import sailpoint.tools.Util;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * Runs during certification generation. It runs once for every CertificationEntity
+ * to determine whether the entity should be pre-delegated to a different certifier. This rule can also be used to
+ * reassign entities to a different certifier. The difference between reassignment and delegation is that reassigned
+ * certifications do not return to the original certifier for review and approval when the assignee has completed
+ * signoff and delegated items do.
+ * <p>
+ * NOTE: This rule runs for each entity identified by the certification specification, so complex processing included
+ * in this rule can significantly impact the performance of certification generation. For continuous certifications,
+ * this rule runs each time the certification returns to the CertificationRequired state and the Refresh Continuous
+ * Certifications task runs.
+ * <p>
+ * Output:
+ * A map of values including the following entries:
+ * • recipient: Identity object to whom the certificationEntity should be delegated (recipient or recipientName must be specified)
+ * • recipientName: (string) name of the recipient identity (alternative to recipient)
+ * • description: (string) description for delegation (if not provided, is generated as “Certify[CertificationEntityName]”)
+ * • comments: (string) comments for the recipient of the delegation/reassignment
+ * • certificationName: (String) name of the new certification to which this entity is being reassigned (only needed for reassignment, not delegation)
+ * • reassign: (Boolean) flag indicating whether this is a reassignment or delegation (true=>reassignment)
+ */
+@Slf4j
+public abstract class CertificationPreDelegationRule
+        extends AbstractJavaRuleExecutor<Map<String, Object>, CertificationPreDelegationRule.CertificationPreDelegationRuleArguments> {
+
+    /**
+     * Name of certification argument name
+     */
+    public static final String ARG_CERTIFICATION_NAME = "certification";
+    /**
+     * Name of entity argument name
+     */
+    public static final String ARG_ENTITY_NAME = "entity";
+    /**
+     * Name of certContext argument name
+     */
+    public static final String ARG_CERT_CONTEXT_NAME = "certContext";
+    /**
+     * Name of state argument name
+     */
+    public static final String ARG_STATE_NAME = "state";
+
+    /**
+     * None nulls arguments
+     */
+    public static final List<String> NONE_NULL_ARGUMENTS_NAME = Arrays.asList(
+            CertificationPreDelegationRule.ARG_CERTIFICATION_NAME,
+            CertificationPreDelegationRule.ARG_ENTITY_NAME,
+            CertificationPreDelegationRule.ARG_CERT_CONTEXT_NAME,
+            CertificationPreDelegationRule.ARG_STATE_NAME
+    );
+
+    /**
+     * Default constructor
+     */
+    public CertificationPreDelegationRule() {
+        super(Rule.Type.CertificationPreDelegation.name(), CertificationPreDelegationRule.NONE_NULL_ARGUMENTS_NAME);
+    }
+
+    /**
+     * Build arguments container for current rule
+     *
+     * @param javaRuleContext - current rule context
+     * @return argument container instance
+     */
+    @Override
+    protected CertificationPreDelegationRule.CertificationPreDelegationRuleArguments buildContainerArguments(
+            @NonNull JavaRuleContext javaRuleContext) {
+        return CertificationPreDelegationRuleArguments.builder()
+                .certification((Certification) JavaRuleExecutorUtil
+                        .getArgumentValueByName(javaRuleContext, CertificationPreDelegationRule.ARG_CERTIFICATION_NAME))
+                .entity((CertificationEntity) JavaRuleExecutorUtil
+                        .getArgumentValueByName(javaRuleContext, CertificationPreDelegationRule.ARG_ENTITY_NAME))
+                .certContext((CertificationContext) JavaRuleExecutorUtil
+                        .getArgumentValueByName(javaRuleContext, CertificationPreDelegationRule.ARG_CERT_CONTEXT_NAME))
+                .state((Map<String, Object>) JavaRuleExecutorUtil
+                        .getArgumentValueByName(javaRuleContext, CertificationPreDelegationRule.ARG_STATE_NAME))
+                .build();
+    }
+
+    /**
+     * Output parameters name
+     */
+    public enum OutputArguments {
+        /**
+         * Identity object to whom the certificationEntity should be delegated (recipient or recipientName must be specified)
+         */
+        RECIPIENT("recipient"),
+        /**
+         * Identity object to whom the certificationEntity should be delegated (recipient or recipientName must be specified)
+         */
+        RECIPIENT_NAME("recipientName"),
+        /**
+         * Description for delegation (if not provided, is generated as “Certify[CertificationEntityName]”)
+         */
+        DESCRIPTION("description"),
+        /**
+         * Comments for the recipient of the delegation/reassignment
+         */
+        COMMENTS("comments"),
+        /**
+         * Name of the new certification to which this entity is being reassigned (only needed for reassignment, not delegation)
+         */
+        CERTIFICATION_NAME("certificationName"),
+        /**
+         * Flag indicating whether this is a reassignment or delegation (true=>reassignment)
+         */
+        REASSIGN("reassign");
+
+        private final String keyValue;
+
+        /**
+         * Constructor with parameters
+         *
+         * @param keyValue - key value for output map
+         */
+        OutputArguments(String keyValue) {
+            this.keyValue = keyValue;
+        }
+
+        /**
+         * Get current key value for output map
+         *
+         * @return key value
+         */
+        public String getKeyValue() {
+            return keyValue;
+        }
+    }
+
+    /**
+     * Result rule builder
+     */
+    @Data
+    @Builder
+    public static final class RuleResult {
+        /**
+         * Identity object to whom the certificationEntity should be delegated (recipient or recipientName must be specified)
+         */
+        private final Identity recipient;
+        /**
+         * Identity object to whom the certificationEntity should be delegated (recipient or recipientName must be specified)
+         */
+        private final String recipientName;
+        /**
+         * Description for delegation (if not provided, is generated as “Certify[CertificationEntityName]”)
+         */
+        private final String description;
+        /**
+         * Comments for the recipient of the delegation/reassignment
+         */
+        private final String comments;
+        /**
+         * Name of the new certification to which this entity is being reassigned (only needed for reassignment, not delegation)
+         */
+        private final String certificationName;
+        /**
+         * Flag indicating whether this is a reassignment or delegation (true=>reassignment)
+         */
+        private final Boolean reassign;
+
+        /**
+         * Convert result to map
+         *
+         * @return map of values
+         */
+        public Map<String, Object> toMap() {
+            Map<String, Object> result = new HashMap<>();
+            addToResult(result, OutputArguments.RECIPIENT.keyValue, recipient);
+            addToResult(result, OutputArguments.RECIPIENT_NAME.keyValue, recipientName);
+            addToResult(result, OutputArguments.DESCRIPTION.keyValue, description);
+            addToResult(result, OutputArguments.COMMENTS.keyValue, comments);
+            addToResult(result, OutputArguments.CERTIFICATION_NAME.keyValue, certificationName);
+            addToResult(result, OutputArguments.REASSIGN.keyValue, reassign);
+            return Util.isEmpty(result) ? Collections.emptyMap() : result;
+        }
+
+        /**
+         * If value is not null - add to result map with key
+         *
+         * @param result - result map
+         * @param key    - key value
+         * @param value  - value to add
+         */
+        private void addToResult(Map<String, Object> result, String key, Object value) {
+            Optional.ofNullable(value).ifPresent(object -> result.put(key, object));
+        }
+    }
+
+    /**
+     * Arguments container for {@link FileParsingRule}. Contains:
+     * - certification
+     * - entity
+     * - certContext
+     * - state
+     */
+    @Data
+    @Builder
+    @ArgumentsContainer
+    public static class CertificationPreDelegationRuleArguments {
+        /**
+         * Reference to the Certification object being created
+         */
+        @Argument(name = CertificationPreDelegationRule.ARG_CERTIFICATION_NAME)
+        private final Certification certification;
+        /**
+         * Reference to the CertificationEntity object being considered for predelegation
+         */
+        @Argument(name = CertificationPreDelegationRule.ARG_ENTITY_NAME)
+        private final CertificationEntity entity;
+        /**
+         * Reference to the CertificationContext interface being  used to create this certification (rarely used within
+         * rules; the values accessible through this are also available on the certification or certificationDefinition)
+         */
+        @Argument(name = CertificationPreDelegationRule.ARG_CERT_CONTEXT_NAME)
+        private final CertificationContext certContext;
+        /**
+         * Map in which any data can be stored; shared across multiple rules in the certification generation process
+         */
+        @Argument(name = CertificationPreDelegationRule.ARG_STATE_NAME)
+        private final Map<String, Object> state;
+    }
+}
---
+++ sail-point-improved/src/test/java/com/sailpoint/improved/rule/CertificationPreDelegationRuleTest.java
@@ -0,0 +1,232 @@
+package com.sailpoint.improved.rule;
+
+import com.sailpoint.improved.rule.certification.CertificationPreDelegationRule;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import sailpoint.api.CertificationContext;
+import sailpoint.api.SailPointContext;
+import sailpoint.api.SailPointFactory;
+import sailpoint.object.Certification;
+import sailpoint.object.CertificationEntity;
+import sailpoint.object.Identity;
+import sailpoint.object.JavaRuleContext;
+import sailpoint.object.Rule;
+import sailpoint.tools.GeneralException;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
+
+import static com.sailpoint.improved.JUnit4Helper.assertThrows;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.CALLS_REAL_METHODS;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Test for {@link CertificationPreDelegationRule} class
+ */
+public class CertificationPreDelegationRuleTest {
+
+    /**
+     * Test instance of {@link CertificationPreDelegationRule}
+     */
+    private CertificationPreDelegationRule testRule;
+
+    /**
+     * Mock of {@link SailPointContext} for returning from {@link SailPointFactory#getCurrentContext()}
+     */
+    private SailPointContext sailPointContext;
+
+    /**
+     * Init {@link CertificationPreDelegationRuleTest#testRule} and {@link CertificationPreDelegationRuleTest#sailPointContext} for test
+     */
+    @Before
+    public void init() {
+        this.sailPointContext = mock(SailPointContext.class);
+        this.testRule = mock(CertificationPreDelegationRule.class,
+                Mockito.withSettings().useConstructor().defaultAnswer(CALLS_REAL_METHODS));
+    }
+
+    /**
+     * Test of normal execution
+     * Input:
+     * - valid rule context
+     * Output:
+     * - test map of execution
+     * Expectation:
+     * - certification as in rule context args by name {@link CertificationPreDelegationRule#ARG_CERTIFICATION_NAME}
+     * - entity as in rule context args by name {@link CertificationPreDelegationRule#ARG_ENTITY_NAME}
+     * - certContext as in rule context args by name {@link CertificationPreDelegationRule#ARG_CERT_CONTEXT_NAME}
+     * - state as in rule context args by name {@link CertificationPreDelegationRule#ARG_STATE_NAME}
+     * - context as in sailpoint context in rule context
+     */
+    @Test
+    public void normalTest() throws GeneralException {
+        JavaRuleContext testRuleContext = buildTestJavaRuleContext();
+        Map<String, Object> testResult = buildValidRuleResult().toMap();
+
+        doAnswer(invocation -> {
+            assertEquals("SailPoint context is not match", testRuleContext.getContext(), invocation.getArguments()[0]);
+            CertificationPreDelegationRule.CertificationPreDelegationRuleArguments arguments = (CertificationPreDelegationRule.CertificationPreDelegationRuleArguments) invocation
+                    .getArguments()[1];
+            assertEquals("Certification is not match",
+                    testRuleContext.getArguments().get(CertificationPreDelegationRule.ARG_CERTIFICATION_NAME),
+                    arguments.getCertification());
+            assertEquals("Entity is not match",
+                    testRuleContext.getArguments().get(CertificationPreDelegationRule.ARG_ENTITY_NAME),
+                    arguments.getEntity());
+            assertEquals("CertificationContext is not match",
+                    testRuleContext.getArguments().get(CertificationPreDelegationRule.ARG_CERT_CONTEXT_NAME),
+                    arguments.getCertContext());
+            assertEquals("State is not match",
+                    testRuleContext.getArguments().get(CertificationPreDelegationRule.ARG_STATE_NAME),
+                    arguments.getState());
+            return testResult;
+        }).when(testRule).internalExecute(eq(sailPointContext), any());
+
+        assertEquals(testResult, testRule.execute(testRuleContext));
+        verify(testRule).internalValidation(eq(testRuleContext));
+        verify(testRule).execute(eq(testRuleContext));
+        verify(testRule).internalExecute(eq(sailPointContext), any());
+    }
+
+    /**
+     * Test execution with invalid arguments.
+     * Input:
+     * - invalid rule context: one of none-null arguments is null
+     * Output:
+     * - General exception
+     * Expectation:
+     * - call internalValidation
+     * - do not call internalExecute
+     */
+    @Test
+    public void noneNullArgumentIsNullTest() throws GeneralException {
+        for (String noneNullArgument : CertificationPreDelegationRule.NONE_NULL_ARGUMENTS_NAME) {
+
+            JavaRuleContext testRuleContext = buildTestJavaRuleContext();
+            testRuleContext.getArguments().remove(noneNullArgument);
+
+            assertThrows(GeneralException.class, () -> testRule.execute(testRuleContext));
+            verify(testRule).internalValidation(eq(testRuleContext));
+            verify(testRule, never()).internalExecute(eq(sailPointContext), any());
+        }
+    }
+
+    /**
+     * Test rule result builder
+     * Input:
+     * - set all values to builder and call toMap
+     * Output:
+     * - map with all key-value pairs
+     * Expectation:
+     * - map contains all keys and values are the same as tests data
+     */
+    @Test
+    public void ruleResultBuilderTest() {
+        CertificationPreDelegationRule.RuleResult ruleResult = buildValidRuleResult();
+        Map<String, Object> resultMap = ruleResult.toMap();
+        assertNotNull("Result map cannot be null", resultMap);
+        for (CertificationPreDelegationRule.OutputArguments outputArgument :
+                CertificationPreDelegationRule.OutputArguments.values()) {
+            assertNotNull("Attribute [" + outputArgument.getKeyValue() + "] cannot be null",
+                    resultMap.get(outputArgument.getKeyValue()));
+        }
+        checkResultMapArguments(CertificationPreDelegationRule.OutputArguments.CERTIFICATION_NAME,
+                ruleResult.getCertificationName(), resultMap);
+        checkResultMapArguments(CertificationPreDelegationRule.OutputArguments.COMMENTS, ruleResult.getComments(),
+                resultMap);
+        checkResultMapArguments(CertificationPreDelegationRule.OutputArguments.DESCRIPTION, ruleResult.getDescription(),
+                resultMap);
+        checkResultMapArguments(CertificationPreDelegationRule.OutputArguments.REASSIGN, ruleResult.getReassign(),
+                resultMap);
+        checkResultMapArguments(CertificationPreDelegationRule.OutputArguments.RECIPIENT, ruleResult.getRecipient(),
+                resultMap);
+        checkResultMapArguments(CertificationPreDelegationRule.OutputArguments.RECIPIENT_NAME,
+                ruleResult.getRecipientName(), resultMap);
+    }
+
+    /**
+     * Test rule result builder
+     * Input:
+     * - set all values to builder to null and call toMap
+     * Output:
+     * - empty map
+     * Expectation:
+     * - map without elements
+     */
+    @Test
+    public void ruleResultBuilderAllArgumentsNullTest() {
+        CertificationPreDelegationRule.RuleResult ruleResult = CertificationPreDelegationRule.RuleResult.builder()
+                .build();
+        Map<String, Object> resultMap = ruleResult.toMap();
+        assertNotNull("Result map cannot be null", resultMap);
+        assertTrue("Result map must be empty", resultMap.isEmpty());
+    }
+
+
+    /**
+     * Check expected value with value from result map after building
+     *
+     * @param argument  - argument to check
+     * @param expected  - expected value
+     * @param resultMap - current result map
+     */
+    private void checkResultMapArguments(CertificationPreDelegationRule.OutputArguments argument, Object expected,
+                                         Map<String, Object> resultMap) {
+        assertEquals(argument.getKeyValue(), expected, resultMap.get(argument.getKeyValue()));
+    }
+
+    /**
+     * Test rule type
+     * Input:
+     * - rule type value
+     * Expectation:
+     * - expected rule type
+     */
+    @Test
+    public void ruleTypeTest() {
+        assertEquals("Rule type is not match", Rule.Type.CertificationPreDelegation.name(), testRule.getRuleType());
+    }
+
+    /**
+     * Create valid java rule context for current rule
+     *
+     * @return valid rule context
+     */
+    private JavaRuleContext buildTestJavaRuleContext() {
+        Map<String, Object> ruleParameters = new HashMap<>();
+        ruleParameters.put(CertificationPreDelegationRule.ARG_CERTIFICATION_NAME, mock(Certification.class));
+        ruleParameters.put(CertificationPreDelegationRule.ARG_ENTITY_NAME, mock(CertificationEntity.class));
+        ruleParameters.put(CertificationPreDelegationRule.ARG_CERT_CONTEXT_NAME, mock(CertificationContext.class));
+        ruleParameters.put(CertificationPreDelegationRule.ARG_STATE_NAME,
+                Collections.singletonMap(UUID.randomUUID().toString(), UUID.randomUUID()));
+        return new JavaRuleContext(this.sailPointContext, ruleParameters);
+    }
+
+    /**
+     * Build rule result with all attributes
+     *
+     * @return valid rule result
+     */
+    private CertificationPreDelegationRule.RuleResult buildValidRuleResult() {
+        return CertificationPreDelegationRule.RuleResult.builder()
+                .certificationName(UUID.randomUUID().toString())
+                .comments(UUID.randomUUID().toString())
+                .description(UUID.randomUUID().toString())
+                .reassign(new Random().nextBoolean())
+                .recipient(mock(Identity.class))
+                .recipientName(UUID.randomUUID().toString())
+                .build();
+    }
+}
