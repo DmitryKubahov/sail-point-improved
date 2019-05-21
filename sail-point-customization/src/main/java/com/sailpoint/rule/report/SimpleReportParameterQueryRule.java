5748a224cc763f7448bb9bc25c8bb23c0470bad4
kubakhov <kubakhov.dmitry@gmail.com>
2019-05-21 16:00:44 +0200

    add simple implementation of ReportParameterQuery rule type

---
+++ sail-point-customization/src/main/java/com/sailpoint/rule/report/SimpleReportParameterQueryRule.java
@@ -0,0 +1,28 @@
+package com.sailpoint.rule.report;
+
+import com.sailpoint.annotation.Rule;
+import com.sailpoint.annotation.common.Argument;
+import com.sailpoint.annotation.common.ArgumentType;
+import com.sailpoint.improved.rule.report.ReportParameterQueryRule;
+import lombok.extern.slf4j.Slf4j;
+import sailpoint.api.SailPointContext;
+import sailpoint.object.QueryOptions;
+
+/**
+ * Simple implementation of {@link ReportParameterQueryRule} rule
+ */
+@Slf4j
+@Rule(value = "Simple report parameter query rule", type = sailpoint.object.Rule.Type.ReportParameterQuery)
+public class SimpleReportParameterQueryRule extends ReportParameterQueryRule {
+
+    /**
+     * Log current query options and return it
+     */
+    @Override
+    @Argument(name = "queryOptions", type = ArgumentType.RETURNS, isReturnsType = true)
+    protected QueryOptions internalExecute(SailPointContext sailPointContext,
+                                           ReportParameterQueryRuleArguments arguments) {
+        log.info("Report query options:[{}]", arguments.getQueryOptions());
+        return arguments.getQueryOptions();
+    }
+}
