85323a9a5d258ab5499d660e2c91785778a1c397
kubakhov <kubakhov.dmitry@gmail.com>
2019-05-21 15:32:48 +0200

    add simple implementation of ReportValidation rule type

---
+++ sail-point-customization/src/main/java/com/sailpoint/rule/report/SimpleReportValidationRule.java
@@ -0,0 +1,32 @@
+package com.sailpoint.rule.report;
+
+import com.sailpoint.annotation.Rule;
+import com.sailpoint.annotation.common.Argument;
+import com.sailpoint.annotation.common.ArgumentType;
+import com.sailpoint.improved.rule.report.ReportValidationRule;
+import lombok.extern.slf4j.Slf4j;
+import sailpoint.api.SailPointContext;
+import sailpoint.tools.Message;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Simple implementation of {@link ReportValidationRule} rule
+ */
+@Slf4j
+@Rule(value = "Simple report validation rule", type = sailpoint.object.Rule.Type.ReportValidator)
+public class SimpleReportValidationRule extends ReportValidationRule {
+
+    /**
+     * Log current live report and form, Return empty list
+     */
+    @Override
+    @Argument(name = "errors", type = ArgumentType.RETURNS, isReturnsType = true)
+    protected List<Message> internalExecute(SailPointContext sailPointContext,
+                                            ReportValidationRuleArguments arguments) {
+        log.info("Live report:[{}]", arguments.getReport());
+        log.info("Report form:[{}]", arguments.getForm());
+        return Collections.emptyList();
+    }
+}
