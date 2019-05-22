821955e4323c5c67916f7afb88543ce96754bb3d
kubakhov <kubakhov.dmitry@gmail.com>
2019-05-22 09:28:15 +0200

    add new rule type -> Escalation

---
+++ sail-point-improved/src/test/java/com/sailpoint/improved/rule/EscalationTest.java
@@ -0,0 +1,130 @@
+package com.sailpoint.improved.rule;
+
+import com.sailpoint.improved.rule.notification.EscalationRule;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import sailpoint.api.SailPointContext;
+import sailpoint.api.SailPointFactory;
+import sailpoint.object.JavaRuleContext;
+import sailpoint.object.Notifiable;
+import sailpoint.object.Rule;
+import sailpoint.tools.GeneralException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static com.sailpoint.improved.JUnit4Helper.assertThrows;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.CALLS_REAL_METHODS;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Test for {@link EscalationRule} class
+ */
+public class EscalationTest {
+
+    /**
+     * Test instance of {@link EscalationRule}
+     */
+    private EscalationRule testRule;
+
+    /**
+     * Mock of {@link SailPointContext} for returning from {@link SailPointFactory#getCurrentContext()}
+     */
+    private SailPointContext sailPointContext;
+
+    /**
+     * Init {@link EscalationTest#testRule} and {@link EscalationTest#sailPointContext} for test
+     */
+    @Before
+    public void init() {
+        this.sailPointContext = mock(SailPointContext.class);
+        this.testRule = mock(EscalationRule.class,
+                Mockito.withSettings().useConstructor().defaultAnswer(CALLS_REAL_METHODS));
+    }
+
+    /**
+     * Test of normal execution
+     * Input:
+     * - valid rule context
+     * Output:
+     * - test object value
+     * Expectation:
+     * - newOwner as in rule context args by name {@link EscalationRule#ARG_ITEM_NAME}
+     * - context as in sailpoint context in rule context
+     */
+    @Test
+    public void normalExecutionTest() throws GeneralException {
+        JavaRuleContext testRuleContext = buildTestJavaRuleContext();
+        String testResult = UUID.randomUUID().toString();
+
+        doAnswer(invocation -> {
+            assertEquals("SailPoint context is not match", testRuleContext.getContext(), invocation.getArguments()[0]);
+            EscalationRule.EscalationRuleArguments arguments = (EscalationRule.EscalationRuleArguments) invocation
+                    .getArguments()[1];
+            assertEquals("Item is not match",
+                    testRuleContext.getArguments().get(EscalationRule.ARG_ITEM_NAME),
+                    arguments.getItem());
+            return testResult;
+        }).when(testRule).internalExecute(eq(sailPointContext), any());
+
+        assertEquals(testResult, testRule.execute(testRuleContext));
+        verify(testRule).internalValidation(eq(testRuleContext));
+        verify(testRule).execute(eq(testRuleContext));
+        verify(testRule).internalExecute(eq(sailPointContext), any());
+    }
+
+    /**
+     * Test execution with invalid arguments.
+     * Input:
+     * - invalid rule context: one of none-null arguments is null
+     * Output:
+     * - General exception
+     * Expectation:
+     * - call internalValidation
+     * - do not call internalExecute
+     */
+    @Test
+    public void noneNullArgumentIsNullTest() throws GeneralException {
+        for (String noneNullArgument : EscalationRule.NONE_NULL_ARGUMENTS_NAME) {
+
+            JavaRuleContext testRuleContext = buildTestJavaRuleContext();
+            testRuleContext.getArguments().remove(noneNullArgument);
+
+            assertThrows(GeneralException.class, () -> testRule.execute(testRuleContext));
+            verify(testRule).internalValidation(eq(testRuleContext));
+            verify(testRule, never()).internalExecute(eq(sailPointContext), any());
+        }
+    }
+
+    /**
+     * Test rule type
+     * Input:
+     * - rule type value
+     * Expectation:
+     * - expected rule type
+     */
+    @Test
+    public void ruleTypeTest() {
+        assertEquals("Rule type is not match", Rule.Type.Escalation.name(),
+                testRule.getRuleType());
+    }
+
+    /**
+     * Create valid java rule context for current rule
+     *
+     * @return valid rule context
+     */
+    private JavaRuleContext buildTestJavaRuleContext() {
+        Map<String, Object> ruleParameters = new HashMap<>();
+        ruleParameters.put(EscalationRule.ARG_ITEM_NAME, mock(Notifiable.class));
+        return new JavaRuleContext(this.sailPointContext, ruleParameters);
+    }
+}
