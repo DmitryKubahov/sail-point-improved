a0183a1be751c8662f528ea65a8d74bc4025b005
kubakhov <kubakhov.dmitry@gmail.com>
2019-05-22 11:03:34 +0200

    add new rule type -> GroupOwner

---
+++ sail-point-improved/src/main/java/com/sailpoint/improved/rule/owner/GroupOwnerRule.java
@@ -0,0 +1,91 @@
+package com.sailpoint.improved.rule.owner;
+
+import com.sailpoint.annotation.common.Argument;
+import com.sailpoint.annotation.common.ArgumentsContainer;
+import com.sailpoint.improved.rule.AbstractJavaRuleExecutor;
+import com.sailpoint.improved.rule.util.JavaRuleExecutorUtil;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import sailpoint.object.GroupDefinition;
+import sailpoint.object.GroupFactory;
+import sailpoint.object.Identity;
+import sailpoint.object.JavaRuleContext;
+import sailpoint.object.Rule;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Used to assign group owners for the groups created from a GroupFactory.
+ * <p>
+ * Output:
+ * Identity object of the Identity assigned as the group owner
+ */
+@Slf4j
+public abstract class GroupOwnerRule
+        extends AbstractJavaRuleExecutor<Identity, GroupOwnerRule.GroupOwnerRuleArguments> {
+
+    /**
+     * Name of factory argument name
+     */
+    public static final String ARG_FACTORY_NAME = "factory";
+    /**
+     * Name of group argument name
+     */
+    public static final String ARG_GROUP_NAME = "group";
+
+    /**
+     * None nulls arguments
+     */
+    public static final List<String> NONE_NULL_ARGUMENTS_NAME = Arrays.asList(
+            GroupOwnerRule.ARG_FACTORY_NAME,
+            GroupOwnerRule.ARG_GROUP_NAME
+    );
+
+    /**
+     * Default constructor
+     */
+    public GroupOwnerRule() {
+        super(Rule.Type.GroupOwner.name(), GroupOwnerRule.NONE_NULL_ARGUMENTS_NAME);
+    }
+
+    /**
+     * Build arguments container for current rule
+     *
+     * @param javaRuleContext - current rule context
+     * @return argument container instance
+     */
+    @Override
+    protected GroupOwnerRule.GroupOwnerRuleArguments buildContainerArguments(
+            @NonNull JavaRuleContext javaRuleContext) {
+        return GroupOwnerRuleArguments.builder()
+                .factory((GroupFactory) JavaRuleExecutorUtil
+                        .getArgumentValueByName(javaRuleContext, GroupOwnerRule.ARG_FACTORY_NAME))
+                .group((GroupDefinition) JavaRuleExecutorUtil
+                        .getArgumentValueByName(javaRuleContext, GroupOwnerRule.ARG_GROUP_NAME))
+                .build();
+    }
+
+    /**
+     * Arguments container for current rule. Contains:
+     * - factory
+     * - group
+     */
+    @Data
+    @Builder
+    @ArgumentsContainer
+    public static class GroupOwnerRuleArguments {
+        /**
+         * Reference to the groupFactory object from which the groups are generated
+         */
+        @Argument(name = GroupOwnerRule.ARG_FACTORY_NAME)
+        private final GroupFactory factory;
+        /**
+         * Reference to a single GroupDefinition from the factory
+         */
+        @Argument(name = GroupOwnerRule.ARG_GROUP_NAME)
+        private final GroupDefinition group;
+    }
+}
