0df598f4e28a494ee90a900111ecf77d48164953
kubakhov <kubakhov.dmitry@gmail.com>
2019-05-22 11:49:53 +0200

    add new rule type -> ScopeCorrelation

---
+++ sail-point-improved/src/main/java/com/sailpoint/improved/rule/scoping/ScopeCorrelationRule.java
@@ -0,0 +1,106 @@
+package com.sailpoint.improved.rule.scoping;
+
+import com.sailpoint.annotation.common.Argument;
+import com.sailpoint.annotation.common.ArgumentsContainer;
+import com.sailpoint.improved.rule.AbstractJavaRuleExecutor;
+import com.sailpoint.improved.rule.util.JavaRuleExecutorUtil;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import sailpoint.object.Identity;
+import sailpoint.object.JavaRuleContext;
+import sailpoint.object.Rule;
+import sailpoint.object.Scope;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Evaluates one or more Identity attributes to select a scope or list of scopes that
+ * applies to the Identity. If it returns multiple scopes, the ScopeSelection rule chooses which of the scopes to
+ * assign. There is only one ScopeCorrelation rule per IdentityIQ installation.
+ * <p>
+ * Output:
+ * Scopes that meet the rule’s criteria for assignment to the Identity
+ */
+@Slf4j
+public abstract class ScopeCorrelationRule
+        extends AbstractJavaRuleExecutor<List<Scope>, ScopeCorrelationRule.ScopeCorrelationRuleArguments> {
+
+    /**
+     * Name of identity argument name
+     */
+    public static final String ARG_IDENTITY_NAME = "identity";
+    /**
+     * Name of scopeCorrelationAttribute argument name
+     */
+    public static final String ARG_SCOPE_CORRELATION_ATTRIBUTE_NAME = "scopeCorrelationAttribute";
+    /**
+     * Name of scopeCorrelationAttributeValue argument name
+     */
+    public static final String ARG_SCOPE_CORRELATION_ATTRIBUTE_VALUE_NAME = "scopeCorrelationAttributeValue";
+
+    /**
+     * None nulls arguments
+     */
+    public static final List<String> NONE_NULL_ARGUMENTS_NAME = Arrays.asList(
+            ScopeCorrelationRule.ARG_IDENTITY_NAME,
+            ScopeCorrelationRule.ARG_SCOPE_CORRELATION_ATTRIBUTE_NAME
+    );
+
+    /**
+     * Default constructor
+     */
+    public ScopeCorrelationRule() {
+        super(Rule.Type.ScopeCorrelation.name(), ScopeCorrelationRule.NONE_NULL_ARGUMENTS_NAME);
+    }
+
+    /**
+     * Build arguments container for current rule
+     *
+     * @param javaRuleContext - current rule context
+     * @return argument container instance
+     */
+    @Override
+    protected ScopeCorrelationRule.ScopeCorrelationRuleArguments buildContainerArguments(
+            @NonNull JavaRuleContext javaRuleContext) {
+        return ScopeCorrelationRuleArguments.builder()
+                .identity((Identity) JavaRuleExecutorUtil
+                        .getArgumentValueByName(javaRuleContext, ScopeCorrelationRule.ARG_IDENTITY_NAME))
+                .scopeCorrelationAttribute((String) JavaRuleExecutorUtil
+                        .getArgumentValueByName(javaRuleContext,
+                                ScopeCorrelationRule.ARG_SCOPE_CORRELATION_ATTRIBUTE_NAME))
+                .scopeCorrelationAttributeValue((String) JavaRuleExecutorUtil
+                        .getArgumentValueByName(javaRuleContext,
+                                ScopeCorrelationRule.ARG_SCOPE_CORRELATION_ATTRIBUTE_VALUE_NAME))
+                .build();
+    }
+
+    /**
+     * Arguments container for current rule. Contains:
+     * - identity
+     * - scopeCorrelationAttribute
+     * - scopeCorrelationAttributeValue
+     */
+    @Data
+    @Builder
+    @ArgumentsContainer
+    public static class ScopeCorrelationRuleArguments {
+        /**
+         * Reference to the identity being assigned a scope
+         */
+        @Argument(name = ScopeCorrelationRule.ARG_IDENTITY_NAME)
+        private final Identity identity;
+        /**
+         * Name of the scope correlation attribute specified in the scoping configuration
+         */
+        @Argument(name = ScopeCorrelationRule.ARG_SCOPE_CORRELATION_ATTRIBUTE_NAME)
+        private final String scopeCorrelationAttribute;
+        /**
+         * The value for the correlation attribute on the Identity
+         */
+        @Argument(name = ScopeCorrelationRule.ARG_SCOPE_CORRELATION_ATTRIBUTE_VALUE_NAME)
+        private final String scopeCorrelationAttributeValue;
+    }
+}
---
+++ sail-point-improved/src/test/java/com/sailpoint/improved/rule/ScopeCorrelationRuleTest.java
@@ -0,0 +1,169 @@
+package com.sailpoint.improved.rule;
+
+import com.sailpoint.improved.rule.scoping.ScopeCorrelationRule;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import sailpoint.api.SailPointContext;
+import sailpoint.api.SailPointFactory;
+import sailpoint.object.Identity;
+import sailpoint.object.JavaRuleContext;
+import sailpoint.object.Rule;
+import sailpoint.object.Scope;
+import sailpoint.tools.GeneralException;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import static com.sailpoint.improved.JUnit4Helper.assertThrows;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.CALLS_REAL_METHODS;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Test for {@link ScopeCorrelationRule} class
+ */
+public class ScopeCorrelationRuleTest {
+
+    /**
+     * Test instance of {@link ScopeCorrelationRule}
+     */
+    private ScopeCorrelationRule testRule;
+
+    /**
+     * Mock of {@link SailPointContext} for returning from {@link SailPointFactory#getCurrentContext()}
+     */
+    private SailPointContext sailPointContext;
+
+    /**
+     * Init {@link ScopeCorrelationRuleTest#testRule} and {@link ScopeCorrelationRuleTest#sailPointContext} for test
+     */
+    @Before
+    public void init() {
+        this.sailPointContext = mock(SailPointContext.class);
+        this.testRule = mock(ScopeCorrelationRule.class,
+                Mockito.withSettings().useConstructor().defaultAnswer(CALLS_REAL_METHODS));
+    }
+
+    /**
+     * Test of normal execution
+     * Input:
+     * - valid rule context
+     * Output:
+     * - test object value
+     * Expectation:
+     * - identity as in rule context args by name {@link ScopeCorrelationRule#ARG_IDENTITY_NAME}
+     * - scopeCorrelationAttribute as in rule context args by name {@link ScopeCorrelationRule#ARG_SCOPE_CORRELATION_ATTRIBUTE_NAME}
+     * - scopeCorrelationAttributeValue as in rule context args by name {@link ScopeCorrelationRule#ARG_SCOPE_CORRELATION_ATTRIBUTE_VALUE_NAME}
+     * - context as in sailpoint context in rule context
+     */
+    @Test
+    public void normalExecutionTest() throws GeneralException {
+        JavaRuleContext testRuleContext = buildTestJavaRuleContext();
+        List<Scope> testResult = Collections.singletonList(mock(Scope.class));
+
+        doAnswer(invocation -> {
+            assertEquals("SailPoint context is not match", testRuleContext.getContext(), invocation.getArguments()[0]);
+            ScopeCorrelationRule.ScopeCorrelationRuleArguments arguments = (ScopeCorrelationRule.ScopeCorrelationRuleArguments) invocation
+                    .getArguments()[1];
+            assertEquals("Identity is not match",
+                    testRuleContext.getArguments().get(ScopeCorrelationRule.ARG_IDENTITY_NAME),
+                    arguments.getIdentity());
+            assertEquals("ScopeCorrelationAttribute is not match",
+                    testRuleContext.getArguments().get(ScopeCorrelationRule.ARG_SCOPE_CORRELATION_ATTRIBUTE_NAME),
+                    arguments.getScopeCorrelationAttribute());
+            assertEquals("ScopeCorrelationAttributeValue is not match",
+                    testRuleContext.getArguments().get(ScopeCorrelationRule.ARG_SCOPE_CORRELATION_ATTRIBUTE_VALUE_NAME),
+                    arguments.getScopeCorrelationAttributeValue());
+            return testResult;
+        }).when(testRule).internalExecute(eq(sailPointContext), any());
+
+        assertEquals(testResult, testRule.execute(testRuleContext));
+        verify(testRule).internalValidation(eq(testRuleContext));
+        verify(testRule).execute(eq(testRuleContext));
+        verify(testRule).internalExecute(eq(sailPointContext), any());
+    }
+
+    /**
+     * Test execution with valid NULL arguments.
+     * Input:
+     * - valid rule context, but {@link ScopeCorrelationRule#ARG_SCOPE_CORRELATION_ATTRIBUTE_VALUE_NAME} attribute null
+     * Output:
+     * - General exception
+     * Expectation:
+     * - call internalValidation
+     * - call internalExecute
+     */
+    @Test
+    public void nullCorrelationAttributeValueArgumentValueTest() throws GeneralException {
+        JavaRuleContext testRuleContext = buildTestJavaRuleContext();
+        List<Scope> testResult = Collections.singletonList(mock(Scope.class));
+
+        testRuleContext.getArguments().remove(ScopeCorrelationRule.ARG_SCOPE_CORRELATION_ATTRIBUTE_VALUE_NAME);
+        when(testRule.internalExecute(eq(sailPointContext), any())).thenReturn(testResult);
+
+        assertEquals(testResult, testRule.execute(testRuleContext));
+        verify(testRule).internalValidation(eq(testRuleContext));
+        verify(testRule).internalExecute(eq(sailPointContext), any());
+    }
+
+
+    /**
+     * Test execution with invalid arguments.
+     * Input:
+     * - invalid rule context: one of none-null arguments is null
+     * Output:
+     * - General exception
+     * Expectation:
+     * - call internalValidation
+     * - do not call internalExecute
+     */
+    @Test
+    public void noneNullArgumentIsNullTest() throws GeneralException {
+        for (String noneNullArgument : ScopeCorrelationRule.NONE_NULL_ARGUMENTS_NAME) {
+
+            JavaRuleContext testRuleContext = buildTestJavaRuleContext();
+            testRuleContext.getArguments().remove(noneNullArgument);
+
+            assertThrows(GeneralException.class, () -> testRule.execute(testRuleContext));
+            verify(testRule).internalValidation(eq(testRuleContext));
+            verify(testRule, never()).internalExecute(eq(sailPointContext), any());
+        }
+    }
+
+    /**
+     * Test rule type
+     * Input:
+     * - rule type value
+     * Expectation:
+     * - expected rule type
+     */
+    @Test
+    public void ruleTypeTest() {
+        assertEquals("Rule type is not match", Rule.Type.ScopeCorrelation.name(),
+                testRule.getRuleType());
+    }
+
+    /**
+     * Create valid java rule context for current rule
+     *
+     * @return valid rule context
+     */
+    private JavaRuleContext buildTestJavaRuleContext() {
+        Map<String, Object> ruleParameters = new HashMap<>();
+        ruleParameters.put(ScopeCorrelationRule.ARG_IDENTITY_NAME, mock(Identity.class));
+        ruleParameters.put(ScopeCorrelationRule.ARG_SCOPE_CORRELATION_ATTRIBUTE_NAME, UUID.randomUUID().toString());
+        ruleParameters
+                .put(ScopeCorrelationRule.ARG_SCOPE_CORRELATION_ATTRIBUTE_VALUE_NAME, UUID.randomUUID().toString());
+        return new JavaRuleContext(this.sailPointContext, ruleParameters);
+    }
+}
