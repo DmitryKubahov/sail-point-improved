c68c596237b5763e935d5761a299479a217234fe
kubakhov <kubakhov.dmitry@gmail.com>
2019-05-22 10:41:30 +0200

    add simple implementation of WorkItemForward rule type

---
+++ sail-point-customization/src/main/java/com/sailpoint/rule/notification/SimpleWorkItemForwardRule.java
@@ -0,0 +1,31 @@
+package com.sailpoint.rule.notification;
+
+import com.sailpoint.annotation.Rule;
+import com.sailpoint.annotation.common.Argument;
+import com.sailpoint.annotation.common.ArgumentType;
+import com.sailpoint.improved.rule.notification.WorkItemForwardRule;
+import lombok.extern.slf4j.Slf4j;
+import sailpoint.api.SailPointContext;
+import sailpoint.object.Identity;
+import sailpoint.tools.GeneralException;
+
+/**
+ * Simple implementation of {@link WorkItemForwardRule} rule
+ */
+@Slf4j
+@Rule(value = "Simple workitem forward rule", type = sailpoint.object.Rule.Type.WorkItemForward)
+public class SimpleWorkItemForwardRule extends WorkItemForwardRule {
+
+    /**
+     * Log current owner and return 'spadmin' identity
+     *
+     * @throws GeneralException if no identity with 'spadmin' name in db
+     */
+    @Override
+    @Argument(name = "newOwner", type = ArgumentType.RETURNS, isReturnsType = true)
+    protected Identity internalExecute(SailPointContext sailPointContext,
+                                       WorkItemForwardRuleArguments arguments) throws GeneralException {
+        log.info("Current owner:{}", arguments.getOwner());
+        return sailPointContext.getObjectByName(Identity.class, "spadmin");
+    }
+}
