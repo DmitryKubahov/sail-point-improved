ac9c778a3eb5333fa48bb23699eb33821d9fe3d6
kubakhov <kubakhov.dmitry@gmail.com>
2019-05-23 09:26:40 +0200

    add simple implementation of AllowedValued rule type

---
+++ sail-point-customization/src/main/java/com/sailpoint/rule/form/SimpleAllowedValuesRule.java
@@ -0,0 +1,30 @@
+package com.sailpoint.rule.form;
+
+import com.sailpoint.annotation.Rule;
+import com.sailpoint.annotation.common.Argument;
+import com.sailpoint.annotation.common.ArgumentType;
+import com.sailpoint.improved.rule.form.AllowedValuesRule;
+import lombok.extern.slf4j.Slf4j;
+import sailpoint.api.SailPointContext;
+
+import java.util.UUID;
+
+/**
+ * Simple implementation of {@link AllowedValuesRule} rule
+ */
+@Slf4j
+@Rule(value = "Simple allowed values rule", type = sailpoint.object.Rule.Type.AllowedValues)
+public class SimpleAllowedValuesRule extends AllowedValuesRule {
+
+    /**
+     * Log current identity, form and field. Return list with random string
+     */
+    @Override
+    @Argument(name = "value", type = ArgumentType.RETURNS, isReturnsType = true)
+    protected String internalExecute(SailPointContext sailPointContext, AllowedValuesRuleArguments arguments) {
+        log.info("Current identity:[{}]", arguments.getIdentity());
+        log.info("Current form:[{}]", arguments.getForm());
+        log.info("Current field:[{}]", arguments.getField());
+        return UUID.randomUUID().toString();
+    }
+}
