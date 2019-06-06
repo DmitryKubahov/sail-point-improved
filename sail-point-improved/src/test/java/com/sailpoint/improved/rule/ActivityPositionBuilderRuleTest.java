bee85beceaec896d3968f75e17a57d343f0227c6
kubakhov <kubakhov.dmitry@gmail.com>
2019-06-06 12:55:22 +0200

    add new rule type -> ActivityPositionBuilder

---
+++ sail-point-improved/src/test/java/com/sailpoint/improved/rule/ActivityPositionBuilderRuleTest.java
@@ -0,0 +1,132 @@
+package com.sailpoint.improved.rule;
+
+import com.sailpoint.improved.rule.activity.ActivityPositionBuilderRule;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import sailpoint.api.SailPointContext;
+import sailpoint.api.SailPointFactory;
+import sailpoint.object.JavaRuleContext;
+import sailpoint.object.Rule;
+import sailpoint.tools.GeneralException;
+
+import java.sql.ResultSet;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static com.sailpoint.improved.JUnit4Helper.assertThrows;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.CALLS_REAL_METHODS;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Test for {@link ActivityPositionBuilderRule} class
+ */
+public class ActivityPositionBuilderRuleTest {
+
+    /**
+     * Test instance of {@link ActivityPositionBuilderRule}
+     */
+    private ActivityPositionBuilderRule testRule;
+
+    /**
+     * Mock of {@link SailPointContext} for returning from {@link SailPointFactory#getCurrentContext()}
+     */
+    private SailPointContext sailPointContext;
+
+    /**
+     * Init {@link ActivityPositionBuilderRuleTest#testRule} and {@link ActivityPositionBuilderRuleTest#sailPointContext} for test
+     */
+    @Before
+    public void init() {
+        this.sailPointContext = mock(SailPointContext.class);
+        this.testRule = mock(ActivityPositionBuilderRule.class,
+                Mockito.withSettings().useConstructor().defaultAnswer(CALLS_REAL_METHODS));
+    }
+
+    /**
+     * Test of normal execution
+     * Input:
+     * - valid rule context
+     * Output:
+     * - test object value
+     * Expectation:
+     * - row as in rule context args by name {@link ActivityPositionBuilderRule#ARG_ROW}
+     * - context as in sailpoint context in rule context
+     */
+    @Test
+    public void normalExecutionTest() throws GeneralException {
+        JavaRuleContext testRuleContext = buildTestJavaRuleContext();
+        Map<String, Object> testResult = Collections.singletonMap(UUID.randomUUID().toString(), UUID.randomUUID());
+
+        doAnswer(invocation -> {
+            assertEquals("JavaRuleContext is not match", testRuleContext, invocation.getArguments()[0]);
+            ActivityPositionBuilderRule.ActivityPositionBuilderRuleArguments arguments = (ActivityPositionBuilderRule.ActivityPositionBuilderRuleArguments) invocation
+                    .getArguments()[1];
+            assertEquals("Row is not match",
+                    testRuleContext.getArguments().get(ActivityPositionBuilderRule.ARG_ROW),
+                    arguments.getRow());
+            return testResult;
+        }).when(testRule).internalExecute(eq(testRuleContext), any());
+
+        assertEquals(testResult, testRule.execute(testRuleContext));
+        verify(testRule).internalValidation(eq(testRuleContext));
+        verify(testRule).execute(eq(testRuleContext));
+        verify(testRule).internalExecute(eq(testRuleContext), any());
+    }
+
+    /**
+     * Test execution with invalid arguments.
+     * Input:
+     * - invalid rule context: one of none-null arguments is null
+     * Output:
+     * - General exception
+     * Expectation:
+     * - call internalValidation
+     * - do not call internalExecute
+     */
+    @Test
+    public void noneNullArgumentIsNullTest() throws GeneralException {
+        for (String noneNullArgument : ActivityPositionBuilderRule.NONE_NULL_ARGUMENTS_NAME) {
+
+            JavaRuleContext testRuleContext = buildTestJavaRuleContext();
+            testRuleContext.getArguments().remove(noneNullArgument);
+
+            assertThrows(GeneralException.class, () -> testRule.execute(testRuleContext));
+            verify(testRule).internalValidation(eq(testRuleContext));
+            verify(testRule, never()).internalExecute(eq(testRuleContext), any());
+        }
+    }
+
+    /**
+     * Test rule type
+     * Input:
+     * - rule type value
+     * Expectation:
+     * - expected rule type
+     */
+    @Test
+    public void ruleTypeTest() {
+        assertEquals("Rule type is not match", Rule.Type.ActivityPositionBuilder.name(),
+                testRule.getRuleType());
+    }
+
+    /**
+     * Create valid java rule context for current rule
+     *
+     * @return valid rule context
+     */
+    private JavaRuleContext buildTestJavaRuleContext() {
+        Map<String, Object> ruleParameters = new HashMap<>();
+        ruleParameters.put(ActivityPositionBuilderRule.ARG_ROW, mock(ResultSet.class));
+        return new JavaRuleContext(this.sailPointContext, ruleParameters);
+    }
+}
+
