e2efca4dac6f8db54738b2668d9693079152c437
kubakhov <kubakhov.dmitry@gmail.com>
2019-06-06 12:10:32 +0200

    add new rule type -> AlertMatch

---
+++ sail-point-improved/src/test/java/com/sailpoint/improved/rule/AlertMatchRuleTest.java
@@ -0,0 +1,130 @@
+package com.sailpoint.improved.rule;
+
+import com.sailpoint.improved.rule.alert.AlertMatchRule;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import sailpoint.api.SailPointContext;
+import sailpoint.api.SailPointFactory;
+import sailpoint.object.Alert;
+import sailpoint.object.JavaRuleContext;
+import sailpoint.object.Rule;
+import sailpoint.tools.GeneralException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import static com.sailpoint.improved.JUnit4Helper.assertThrows;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.CALLS_REAL_METHODS;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Test for {@link AlertMatchRule} class
+ */
+public class AlertMatchRuleTest {
+
+    /**
+     * Test instance of {@link AlertMatchRule}
+     */
+    private AlertMatchRule testRule;
+
+    /**
+     * Mock of {@link SailPointContext} for returning from {@link SailPointFactory#getCurrentContext()}
+     */
+    private SailPointContext sailPointContext;
+
+    /**
+     * Init {@link AlertMatchRuleTest#testRule} and {@link AlertMatchRuleTest#sailPointContext} for test
+     */
+    @Before
+    public void init() {
+        this.sailPointContext = mock(SailPointContext.class);
+        this.testRule = mock(AlertMatchRule.class,
+                Mockito.withSettings().useConstructor().defaultAnswer(CALLS_REAL_METHODS));
+    }
+
+    /**
+     * Test of normal execution
+     * Input:
+     * - valid rule context
+     * Output:
+     * - test object value
+     * Expectation:
+     * - alert as in rule context args by name {@link AlertMatchRule#ARG_ALERT}
+     * - context as in sailpoint context in rule context
+     */
+    @Test
+    public void normalExecutionTest() throws GeneralException {
+        JavaRuleContext testRuleContext = buildTestJavaRuleContext();
+        Boolean testResult = new Random().nextBoolean();
+
+        doAnswer(invocation -> {
+            assertEquals("JavaRuleContext is not match", testRuleContext, invocation.getArguments()[0]);
+            AlertMatchRule.AlertMatchRuleArguments arguments = (AlertMatchRule.AlertMatchRuleArguments) invocation
+                    .getArguments()[1];
+            assertEquals("Alert is not match",
+                    testRuleContext.getArguments().get(AlertMatchRule.ARG_ALERT),
+                    arguments.getAlert());
+            return testResult;
+        }).when(testRule).internalExecute(eq(testRuleContext), any());
+
+        assertEquals(testResult, testRule.execute(testRuleContext));
+        verify(testRule).internalValidation(eq(testRuleContext));
+        verify(testRule).execute(eq(testRuleContext));
+        verify(testRule).internalExecute(eq(testRuleContext), any());
+    }
+
+    /**
+     * Test execution with invalid arguments.
+     * Input:
+     * - invalid rule context: one of none-null arguments is null
+     * Output:
+     * - General exception
+     * Expectation:
+     * - call internalValidation
+     * - do not call internalExecute
+     */
+    @Test
+    public void noneNullArgumentIsNullTest() throws GeneralException {
+        for (String noneNullArgument : AlertMatchRule.NONE_NULL_ARGUMENTS_NAME) {
+
+            JavaRuleContext testRuleContext = buildTestJavaRuleContext();
+            testRuleContext.getArguments().remove(noneNullArgument);
+
+            assertThrows(GeneralException.class, () -> testRule.execute(testRuleContext));
+            verify(testRule).internalValidation(eq(testRuleContext));
+            verify(testRule, never()).internalExecute(eq(testRuleContext), any());
+        }
+    }
+
+    /**
+     * Test rule type
+     * Input:
+     * - rule type value
+     * Expectation:
+     * - expected rule type
+     */
+    @Test
+    public void ruleTypeTest() {
+        assertEquals("Rule type is not match", Rule.Type.AlertMatch.name(),
+                testRule.getRuleType());
+    }
+
+    /**
+     * Create valid java rule context for current rule
+     *
+     * @return valid rule context
+     */
+    private JavaRuleContext buildTestJavaRuleContext() {
+        Map<String, Object> ruleParameters = new HashMap<>();
+        ruleParameters.put(AlertMatchRule.ARG_ALERT, mock(Alert.class));
+        return new JavaRuleContext(this.sailPointContext, ruleParameters);
+    }
+}
